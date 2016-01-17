6d2390a17c0ca333669f23aeba62d7d38a10f223
Object Arts <info@object-arts.com>
2016-01-17 22:52:02 +0000

    Check in with a failure for to validate.

--- BootDPRO+Test.cmd
+++ BootDPRO+Test.cmd
@@ -3,12 +3,16 @@ Dolphin7 DBOOT.img7 DolphinProfessional
 
 IF %ERRORLEVEL% NEQ 0 (
   ECHO Boot failed, Code=%ERRORLEVEL%
-  PAUSE
   EXIT %ERRORLEVEL%
 )
 
 ECHO Running regression tests
 START /Wait Dolphin7 DPRO.img7 -f TestDPRO.st -q
+
+IF %ERRORLEVEL% NEQ 0 (
+  ECHO Tests failed=%ERRORLEVEL%
+  EXIT %ERRORLEVEL%
+)
 
 ECHO Finished regression tests
 
