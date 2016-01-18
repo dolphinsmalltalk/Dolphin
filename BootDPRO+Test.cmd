258013de2a50da1412646d562dd2492f2f7728fd
Object Arts <info@object-arts.com>
2016-01-18 06:11:32 +0000

    Try again

--- BootDPRO+Test.cmd
+++ BootDPRO+Test.cmd
@@ -8,8 +8,13 @@ IF %ERRORLEVEL% NEQ 0 (
 
 ECHO Running regression tests
 START /Wait Dolphin7 DPRO.img7 -f TestDPRO.st -q
-TYPE DPRO.testlog
 
+IF %ERRORLEVEL% NEQ 0 (
+  TYPE DPRO.testlog
+  ECHO Tests failed=%ERRORLEVEL%
+  EXIT -1
+)
+
+TYPE DPRO.testlog
 ECHO Finished regression tests
-EXIT %ERRORLEVEL%
 
