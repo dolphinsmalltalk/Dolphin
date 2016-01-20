63b14ea718d47b6cdf2a6ba7641cd963307834d8
Object Arts <info@object-arts.com>
2016-01-20 21:40:50 +0000

    AppVeyor runs boot image and tests

---
+++ AppVeyorTestDolphinVM.cmd
@@ -0,0 +1,11 @@
+git clone -q --branch=master https://github.com/dolphinsmalltalk/Dolphin.git Dolphin
+
+copy ..\..\Dolphin7.exe Dolphin
+copy ..\..\DolphinVM7.dll Dolphin
+copy ..\..\DolphinCR7.dll Dolphin
+copy ..\..\DolphinDR7.dll Dolphin
+copy ..\..\DolphinSureCrypto.dll Dolphin
+
+cd Dolphin
+CALL BootDPRO
+CALL TestDPRO
