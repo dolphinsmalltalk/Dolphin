733d6e4b77217278d4c8ff5554ca66a6de2bd99b
Object Arts <info@object-arts.com>
2016-01-20 22:02:30 +0000

    AppVeyor build and acceptance tests now run green

--- AppVeyorTestDolphinVM.cmd
+++ AppVeyorTestDolphinVM.cmd
@@ -1,12 +1,15 @@
-@ECHO ON
+ECHO Clone Dolphin image environment
 git clone -q --branch=master https://github.com/dolphinsmalltalk/Dolphin.git Dolphin
 
+
+ECHO Copy executables
 copy ..\..\Dolphin7.exe Dolphin
 copy ..\..\DolphinVM7.dll Dolphin
 copy ..\..\DolphinCR7.dll Dolphin
 copy ..\..\DolphinDR7.dll Dolphin
 copy ..\..\DolphinSureCrypto.dll Dolphin
 
+ECHO Boot and test image
 cd Dolphin
 CALL BootDPRO
 CALL TestDPRO
