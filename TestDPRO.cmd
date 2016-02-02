7b91dad62f5a400f9a4429ab80272b383af5fa2f
Object Arts <info@object-arts.com>
2016-02-02 17:19:48 +0000

    Use contents of .lestlog file to determine success or failure rather than relying on exit code from Dolphin. The move to VS2015 seems to have exacerbated a situation where the return code from SessionManager>>quit: may not make it back to the shell. For the the time being we'll change the way we detect success or failure of the tests.

--- TestDPRO.cmd
+++ TestDPRO.cmd
@@ -1,13 +1,12 @@
 @ECHO OFF
 ECHO Running regression tests
 START /Wait Dolphin7 DPRO.img7 -f RegressionTestsRun.st -q
+FINDSTR /L /C:" 0 errors" DPRO.testlog
 
 IF %ERRORLEVEL% NEQ 0 (
   TYPE DPRO.testlog
-  ECHO Tests failed=%ERRORLEVEL%
+  ECHO Tests failed
   EXIT -1
 )
 
-TYPE DPRO.testlog
 ECHO Finished regression tests
-
