867e866aed6954b15240a53112c2462d64c864a2
James Foster <github@jgfoster.net>
2016-02-02 08:45:32 -0800

    Simplify test script.

--- TestDPRO.cmd
+++ TestDPRO.cmd
@@ -1,13 +1,6 @@
 @ECHO OFF
 ECHO Running regression tests
 START /Wait Dolphin7 DPRO.img7 -f RegressionTestsRun.st -q
-
-IF %ERRORLEVEL% NEQ 0 (
-  TYPE DPRO.testlog
-  ECHO Tests failed=%ERRORLEVEL%
-  EXIT -1
-)
-
+set errorCode=%ERRORLEVEL%
 TYPE DPRO.testlog
-ECHO Finished regression tests
-
+EXIT errorCode
