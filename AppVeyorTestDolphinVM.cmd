31c7212e044b8a587149ea9affa37ccf105f157d
Object Arts <info@object-arts.com>
2016-02-13 12:24:25 +0000

    Fix failing VersionResourceTests caused by removal of EXE/DLLs from image repo.

--- AppVeyorTestDolphinVM.cmd
+++ AppVeyorTestDolphinVM.cmd
@@ -2,11 +2,8 @@ ECHO Clone Dolphin image environment
 git clone -q --branch=master https://github.com/dolphinsmalltalk/Dolphin.git Dolphin
 
 ECHO Copy executables
-copy ..\..\Dolphin7.exe Dolphin
-copy ..\..\DolphinVM7.dll Dolphin
-copy ..\..\DolphinCR7.dll Dolphin
-copy ..\..\DolphinDR7.dll Dolphin
-copy ..\..\DolphinSureCrypto.dll Dolphin
+copy ..\..\*.exe Dolphin
+copy ..\..\*.dll Dolphin
 
 ECHO Boot and test image
 cd Dolphin
