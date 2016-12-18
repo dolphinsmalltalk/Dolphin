c0d0dec8361341d152a0b7966c37800b65404cdc
Blair McGlashan <blairmcg@outlook.com>
2016-12-18 11:34:40 +0000

    Produce detailed VM dump log on boot or regression test run failure.

--- TestDPRO.cmd
+++ TestDPRO.cmd
@@ -2,7 +2,7 @@
 ECHO Running regression tests
 echo. >DPRO.errors
 echo Fatal error terminated run: >DPRO.testlog
-START /B /Wait Dolphin7 DPRO.img7 -u -f RegressionTestsRun.st -q
+Dolphin7 DPRO.img7 -u -f RegressionTestsRun.st -q
 FINDSTR /L /C:"PASSED" DPRO.testlog >nul
 set errorCode=%ERRORLEVEL%
 TYPE DPRO.testlog
