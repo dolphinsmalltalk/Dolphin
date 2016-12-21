74a2c77d206aa6b14a6c29a691528504f252605e
Blair McGlashan <blairmcg@outlook.com>
2016-12-21 17:12:42 +0000

    Fix #116: Regression tests are getting slow, but difficult to tell which...

--- TestDPRO.cmd
+++ TestDPRO.cmd
@@ -1,10 +1,7 @@
 @ECHO OFF
 ECHO Running regression tests
 echo. >DPRO.errors
-echo Fatal error terminated run: >DPRO.testlog
 Dolphin7 DPRO.img7 -u -f RegressionTestsRun.st -q
-FINDSTR /L /C:"PASSED" DPRO.testlog >nul
 set errorCode=%ERRORLEVEL%
-TYPE DPRO.testlog
 TYPE DPRO.errors
 EXIT /b %errorCode%
