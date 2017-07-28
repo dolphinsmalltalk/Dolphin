8ccb02ae56ed6ae6ab9fc64eb4bd53e2216a4ac0
Blair McGlashan <blairmcg@outlook.com>
2017-07-28 19:41:03 +0100

    Upload test results file to appveyor

---
+++ UploadTestResults.ps1
@@ -0,0 +1,4 @@
+# Powershell script to upload the regression tests results to AppVeyor
+
+$wc = New-Object 'System.Net.WebClient'
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\RegressionTests.xml))
