49a670ab28c2da59273900a8a0430f2873bd35c3
Blair McGlashan <blairmcg@outlook.com>
2017-07-28 19:41:03 +0100

    Upload test results file to appveyor

--- TestDPRO.cmd
+++ TestDPRO.cmd
@@ -4,4 +4,5 @@ echo. >DPRO.errors
 Dolphin7 DPRO.img7 -u -f RegressionTestsRun.st -q
 set errorCode=%ERRORLEVEL%
 TYPE DPRO.errors
+if NOT "%APPVEYOR%"=="" powershell.exe -file UploadTestResults.ps1
 EXIT /b %errorCode%
