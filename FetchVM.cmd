8a92554728d18d1d78f1651b602bc2e88755ba81
Blair <blairmcg@outlook.com>
2017-08-29 18:46:33 +0100

    Skip tests of lexical errors for bad/missing code point for older compiler
    Next version of compiler has better error reporting for malformed
    escaped literal characters to match SmalltalkParser.

--- FetchVM.cmd
+++ FetchVM.cmd
@@ -3,4 +3,4 @@ REM Fetch the DolphinVM binaries from GitHub into the current directory.
 REM Specify a parameter to fetch a specific version or leave blank to
 REM get the recommended VM version for this release of the image.
 
-powershell.exe -file FetchVM.ps1 %*
+powershell.exe -ExecutionPolicy RemoteSigned -file FetchVM.ps1 %*
