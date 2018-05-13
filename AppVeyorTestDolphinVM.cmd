405ec0e005f7ada1eb135b3a19db7306da2fa074
Blair McGlashan <blairmcg@outlook.com>
2018-05-13 16:06:36 +0100

    Oops, forgot to retarget to master

--- AppVeyorTestDolphinVM.cmd
+++ AppVeyorTestDolphinVM.cmd
@@ -1,5 +1,5 @@
 ECHO Clone Dolphin image environment
-git clone -q --branch=7.0 https://github.com/dolphinsmalltalk/Dolphin.git Dolphin
+git clone -q --branch=master https://github.com/dolphinsmalltalk/Dolphin.git Dolphin
 
 ECHO Copy executables
 copy ..\..\*.exe Dolphin
