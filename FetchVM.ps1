4031849bdb309ddac6bd61c66402afcc77372241
Blair McGlashan <blairmcg@outlook.com>
2018-06-18 20:38:24 +0100

    Update FetchVM script to fetch 7.1.1

--- FetchVM.ps1
+++ FetchVM.ps1
@@ -3,7 +3,7 @@
 
 param 
 (
-    [string]$VMversion="v7.1.0"
+    [string]$VMversion="v7.1.1"
 )
 
 # Override Powershell's default use of TLS1.0 for web requests; this is insecure and no longer works with GitHub
