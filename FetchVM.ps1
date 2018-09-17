8763b36d06627be510ace25bb334f6bc0cc0819a
Blair McGlashan <blairmcg@outlook.com>
2018-09-17 21:04:37 +0100

    Prepare to publish 7.1.3

--- FetchVM.ps1
+++ FetchVM.ps1
@@ -3,7 +3,7 @@
 
 param 
 (
-    [string]$VMversion="v7.1.2"
+    [string]$VMversion="v7.1.3"
 )
 
 # Override Powershell's default use of TLS1.0 for web requests; this is insecure and no longer works with GitHub
