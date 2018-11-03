cf01e37b0bc98376ba19780941549911368956da
Blair McGlashan <blairmcg@outlook.com>
2018-11-03 11:41:16 +0000

    Remove scripts for booting obsolete core configuration
    Relates to #662

--- BootAll.cmd
+++
@@ -1,2 +0,0 @@
-CALL BootCore
-CALL BootDPRO
--- BootCore.cmd
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-Dolphin7 DBOOT.img7 DolphinCoreProduct
-IF %ERRORLEVEL% NEQ 0 (
-  ECHO Boot failed, Code=%ERRORLEVEL%
-  if "%AppVeyor%"=="" PAUSE
-)
