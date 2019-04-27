7e99ad788d73a83a950b3bc9a2029cb8f01376c0
Blair McGlashan <blairmcg@outlook.com>
2019-04-27 11:11:14 +0100

    Update FetchVM to pull 7.1.4 VM, and ensure this is not needed in future
    FetchVM.cmd now uses the latest tag to determine the version of the VM to
    fetch, so it should not need to be updated for future releases.

--- FetchVM.cmd
+++ FetchVM.cmd
@@ -3,4 +3,5 @@ REM Fetch the DolphinVM binaries from GitHub into the current directory.
 REM Specify a parameter to fetch a specific version or leave blank to
 REM get the recommended VM version for this release of the image.
 
-powershell.exe -ExecutionPolicy RemoteSigned -file FetchVM.ps1 %*
+FOR /F "usebackq delims=" %%A in (`"git describe --abbrev=0"`) do SET tag=%%A
+powershell.exe -ExecutionPolicy RemoteSigned -file FetchVM.ps1 %tag% %*
