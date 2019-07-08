dolphinsmalltalk/Dolphin

commit 5634a618f1887d29ce82285dd36f1809c0f4486a

Debug build cast-to-smaller-size traps from new identity hash calc

Core/DolphinVM/ote.h
@@ -151,13 +151,9 @@ public:
 	__forceinline OTEFlags::Spaces heapSpace() const		{ return static_cast<OTEFlags::Spaces>(m_flags.m_space); }
 	__forceinline bool flagsAllMask(BYTE mask) const		{ return (m_ubFlags & mask) == mask; }
 
-	hash_t identityHash()
+	__forceinline hash_t identityHash()
 	{
-		while (m_idHash == 0)
-		{
-			m_idHash = ObjectMemory::nextIdentityHash();
-		}
-		return m_idHash;
+		return m_idHash != 0 ? m_idHash : m_idHash = ObjectMemory::nextIdentityHash();
 	}
 
 public:

Core/DolphinVM/objmem.h
@@ -1081,11 +1081,14 @@ inline bool ObjectMemory::IsConstObj(void* ptr)
 
 inline hash_t ObjectMemory::nextIdentityHash()
 {
-	hash_t y = LOWORD(m_nNextIdHash);
-	hash_t x = HIWORD(m_nNextIdHash);
-	hash_t t = x ^ (x << 5);
+	uint32_t seed = m_nNextIdHash;
+	hash_t y = LOWORD(seed);
+	hash_t x = HIWORD(seed);
+	// The 16-bit masks make no difference to the code generated in a release build (they are redundant),
+	// but prevent a debug report of loss of bits when casting to a smaller sized int in a debug build
+	hash_t t = x ^ static_cast<hash_t>((x << 5) & 0xffff);
 	m_nNextIdHash = y << 16 | ((y ^ (y >> 1)) ^ (t ^ (t >> 3)));
-	return static_cast<uint16_t>(m_nNextIdHash);
+	return static_cast<uint16_t>(m_nNextIdHash & 0xffff);
 }
 
 ///////////////////////////////////////////////////////////////////////////////

commit d6c3eae1a7b652410f1560c9c0705a2b46dfaf06

Identity hash function must be called in a loop to avoid transient zero
The new identity hash function uses 16-bits of a 32-bit hash. Very
infrequently the 16-bit value can be zero, which is the same as the
initial value for an object that has not yet been allocated a hash, causing
a further allocation to occur the next time the identity hash is requested.
This resulted in an occasional failure of IdeaSpaceShellTests
testHistoryRemove, which could go away with code changes.
This bug was introduced by 5634a618

Core/DolphinVM/ote.h
@@ -153,7 +153,12 @@ public:
 
 	__forceinline hash_t identityHash()
 	{
-		return m_idHash != 0 ? m_idHash : m_idHash = ObjectMemory::nextIdentityHash();
+		// This needs to be a loop in case nextIdentityHash ever returns zero
+		while (m_idHash == 0)
+		{
+			m_idHash = ObjectMemory::nextIdentityHash();
+		}
+		return m_idHash;
 	}
 
 public: