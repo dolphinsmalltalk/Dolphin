43b28b6b42b7037294d4b45894d67ed0eb2782f6
Blair McGlashan <blairmcg@outlook.com>
2019-07-11 20:56:30 +0100

    Use unannotated tags for fetchvm as github creates these for releases
    The FetchVM script uses the latest tag to decide which VM to download, but
    was assuming that only annotated tags should be considered. However, since
    the tags created by github releases are not annotated, this is not very
    useful!

--- FetchVM.cmd
+++ FetchVM.cmd
@@ -3,5 +3,5 @@ REM Fetch the DolphinVM binaries from GitHub into the current directory.
 REM Specify a parameter to fetch a specific version or leave blank to
 REM get the recommended VM version for this release of the image.
 
-FOR /F "usebackq delims=" %%A in (`"git describe --abbrev=0"`) do SET tag=%%A
+FOR /F "usebackq delims=" %%A in (`"git describe --tags --abbrev=0"`) do SET tag=%%A
 powershell.exe -ExecutionPolicy RemoteSigned -file FetchVM.ps1 %tag% %*
