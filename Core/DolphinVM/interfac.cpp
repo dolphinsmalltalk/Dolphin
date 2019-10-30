dolphinsmalltalk/Dolphin

commit 9ffb339942f86cc8a37b060dac0dc0e581f7d51e

Remove primitiveFailureData and simplify overlapped call mechanism

Core/DolphinVM/interfac.cpp
@@ -428,8 +428,6 @@ void Interpreter::MarkRoots()
 		ObjectMemory::MarkObjectsAccessibleFromRoot(*m_roots[i]);
 		i++;
 	}
-
-	OverlappedCall::MarkRoots();
 }
 
 // A compacting GC has occurred, ask ObjectMemory to update any stored down Oops
@@ -456,9 +454,21 @@ void Interpreter::OnCompact()
 	ASSERT(ObjectMemory::isKindOf(m_registers.m_oteActiveProcess, Pointers.ClassProcess));
 
 	flushCaches();
+}
 
-	OverlappedCall::OnCompact();
-	//compiler->onCompact();
+void Interpreter::CompactVirtualObject(OTE* ote)
+{
+	// TODO: Uncommit any virtual memory that is not currently in use by the object (i.e. shrink process stacks)
+
+	if (ote->m_oteClass == Pointers.ClassProcess)
+	{
+		ProcessOTE* oteProcess = reinterpret_cast<ProcessOTE*>(ote);
+		OverlappedCallPtr oc = oteProcess->m_location->GetOverlappedCall();
+		if (oc)
+		{
+			oc->OnCompact();
+		}
+	}
 }
 
 #pragma code_seg()

Core/DolphinVM/thrdcall.h
@@ -18,7 +18,6 @@
 #define TRACING 0
 #endif
 
-
 template<class T> class SmartPtr
 {
 	T* m_pObj;
@@ -53,6 +52,12 @@ public:
 		AddRef();
 	}
 
+	// Move constructor
+	SmartPtr(SmartPtr&& other) : m_pObj(other.m_pObj)
+	{
+		other.m_pObj = nullptr;
+	}
+
 	// Destructor
 	~SmartPtr()
 	{
@@ -72,6 +77,17 @@ public:
 	SmartPtr& operator=(const SmartPtr& cp)
 	{ 
 		return operator=(cp.m_pObj); 
+	}
+
+	SmartPtr& operator=(SmartPtr&& other) 
+	{
+		if (this != &other) 
+		{
+			Release();
+			m_pObj = other.m_pObj;
+			other.m_pObj = nullptr;
+		}
+		return *this;
 	}
 
 	T* operator->() const
@@ -100,75 +116,7 @@ public:
 class OverlappedCall;
 typedef SmartPtr<OverlappedCall> OverlappedCallPtr;
 
-template <class T> class DoubleLink
-{
-	T* prev;
-	T* next;
-
-public:
-	DoubleLink() : prev(NULL), next(NULL) {}
-
-	T* Next() const { return next; }
-
-	T* Unlink()
-	{
-		// next null if last link in list
-		if (next)
-			next->prev = prev;
-		// prev pointer normally valid because there is an anchor node, but when shutting
-		// down will already have been unlinked from the list
-		if (prev)
-			prev->next = next;
-		next = prev = NULL;
-
-		return static_cast<T*>(this);
-	}
-
-	void LinkAfter(T* prevArg)
-	{
-		T* pThis = static_cast<T*>(this);
-		this->next = prevArg->next;
-		if (this->next)
-			this->next->prev = pThis;
-		this->prev = prevArg;
-		prevArg->next = pThis;
-	}
-};
-
-template <class T> class DoublyLinkedList
-{
-	DoubleLink<T> anchor;
-public:
-	T* First() const
-	{ 
-		return anchor.Next(); 
-	}
-
-	bool IsEmpty() const
-	{
-		return First() == NULL;
-	}
-
-	T* RemoveFirst()
-	{
-		T* node = First();
-		if (node != NULL)
-			node->Unlink();
-		return node;
-	}
-
-	void AddFirst(T* node)
-	{
-		// Slightly nasty thing is that anchor node is not actually the same type as
-		// the other nodes, so we need a cast here.
-		node->LinkAfter(reinterpret_cast<T*>(&anchor));
-	}
-};
-
-class OverlappedCall;
-typedef DoublyLinkedList<OverlappedCall> OverlappedCallList;
-
-class OverlappedCall : public DoubleLink<OverlappedCall>
+class OverlappedCall
 {
 public:
 	// Allocate/free methods for maintaining pool of available blocks
@@ -181,11 +129,9 @@ public:
 	DWORD AddRef();
 	DWORD Release();
 
-	enum States { Starting, Resting, Running, Terminated };
+	enum States { Starting, Resting, Terminated, Calling, Returned };
 
-	bool beStarted();
 	States beTerminated();
-	bool beResting();
 
 private:
 	///////////////////////////////////////////////////////////////////////////
@@ -194,21 +140,26 @@ private:
 	OverlappedCall(ProcessOTE*);
 	~OverlappedCall();
 
-	Process* GetProcess() { return m_oteProcess->m_location; }
+	Process* GetProcess() 
+	{ 
+		HARDASSERT((POTE)m_oteProcess != Pointers.Nil);
+		return m_oteProcess->m_location; 
+	}
 
 	// Start corresponding OS thread
 	bool BeginThread();
 
 	void Init();
 	void Term();
+	void Free();
 
 	void TerminateThread();
 
 	bool Initiate(CompiledMethod* pMethod, unsigned nArgCount);
 	DWORD WaitForRequest();
 	int ProcessRequests();
-	void PerformCall();
-	void CallFinished();
+	bool PerformCall();
+	bool CallFinished();
 
 	int Main();
 	int TryMain();
@@ -232,8 +183,6 @@ private:
 	bool QueueForInterpreter(PAPCFUNC pfnAPC);
 	bool QueueForMe(PAPCFUNC pfnAPC);
 
-	void compact();
-
 public:
 	///////////////////////////////////////////////////////////////////////////
 	// Static member functions
@@ -241,26 +190,20 @@ public:
 	static void Initialize();
 	static void Uninitialize();
 
-	static OverlappedCall* GetActiveProcessOverlappedCall();
-	static OverlappedCall* Do(CompiledMethod* pMethod, unsigned argCount);
+	static OverlappedCallPtr GetActiveProcessOverlappedCall();
+	static OverlappedCallPtr Do(CompiledMethod* pMethod, unsigned argCount);
 
 	static Semaphore* pendingTerms();
 
-	static void MarkRoots();
-	static void OnCompact();
-
 	void OnActivateProcess();
-
-	#ifdef _DEBUG
-		static void ReincrementProcessReferences();
-	#endif
+	void OnCompact();
 
 	bool IsInCall();
 
 private:
 	// Low-level management routines
-	static OverlappedCall* New(ProcessOTE*);
-	static OverlappedCallPtr RemoveFirstFromList(OverlappedCallList&);
+	static OverlappedCallPtr New(ProcessOTE*);
+	//static OverlappedCallPtr RemoveFirstFromList(OverlappedCallList&);
 
 	// Thread entry point function
 	static unsigned __stdcall ThreadMain(void* pThis);
@@ -272,17 +215,13 @@ private:
 	static OverlappedCallPtr BeginAPC(DWORD dwParam);
 	static OverlappedCallPtr BeginMainThreadAPC(DWORD dwParam);
 
-	static void CompactCallsOnList(OverlappedCallList& list);
+	//static void CompactCallsOnList(OverlappedCallList& list);
 
 	///////////////////////////////////////////////////////////////////////////
 	// Static data members
 private:
 
-	// Low-level pool management data
-//	static int nFirstFree;
-//	static int nMaxFree;
-	static CMonitor s_listMonitor;
-	static OverlappedCallList s_activeList;
+	//static OverlappedCallList s_activeList;
 	
 	static bool s_bShutdown;
 
@@ -293,10 +232,9 @@ public:
 		tracestream thinDump;
 	#endif
 
-private:
 	// Context of the process for which we are running
 	InterpreterRegisters	m_interpContext;
-	ProcessOTE*				m_oteProcess;
+	ProcessOTE*				m_oteProcess;	// paired Smalltalk Process
 	SHAREDLONG				m_dwRefs;
 
 	HANDLE					m_hThread;		// thread handle (returned by _beginthread(ex))
@@ -308,12 +246,9 @@ private:
 	CompiledMethod*			m_pMethod;
 	unsigned				m_nArgCount;
 	volatile States			m_state;
-public:
-	SHAREDLONG				m_nCallDepth;
 private:
 	SHAREDLONG				m_nSuspendCount;
-	bool					m_bCompletionRequestPending;
-	bool					m_bCallPrimitiveFailed;
+	_PrimitiveFailureCode	m_primitiveFailureCode;
 };
 
 
@@ -335,5 +270,5 @@ inline DWORD OverlappedCall::Release()
 inline bool OverlappedCall::IsInCall()
 {
 	HARDASSERT(::GetCurrentThreadId() == Interpreter::MainThreadId());
-	return m_nCallDepth > 0;
+	return m_state >= Calling;
 }

Core/DolphinVM/vmref.cpp
@@ -119,11 +119,6 @@ void Interpreter::RemoveVMReference(OTE* pOTE)
 		m_oteNewProcess->countUp();					// new process or nil
 		m_oteUnderConstruction->countUp();			// Window currently under construction or nil
 		
-		OverlappedCall::ReincrementProcessReferences();
-
-		//m_oopMessageSelector = Pointers.Nil;
-		//m_oopNewMethod = Pointers.Nil;
-		
 		ObjectMemory::addVMRefs();
 	}
 #endif

Core/Object Arts/Dolphin/IDE/Base/VMTest.cls
@@ -43,8 +43,15 @@ assertImmutableResize: anObject
 	#(0 1 -1)
 		do: [:i | self should: [anObject resize: anObject size + i] raise: Processor constWriteSignal]!
 
+compile: text in: aBehavior
+	| method |
+	method := Compiler compile: text in: aBehavior.
+	self deny: method identicalTo: nil.
+	method sourceDescriptor: text.
+	^method!
+
 createPrimitiveMethod: anInteger in: aBehavior argCount: argc setsFailCode: aBoolean
-	| method text |
+	| text |
 	text := String writeStream.
 	argc isZero
 		ifTrue: [text nextPutAll: 'prim ']
@@ -63,11 +70,7 @@ createPrimitiveMethod: anInteger in: aBehavior argCount: argc setsFailCode: aBoo
 		nextPutAll: (aBoolean
 					ifTrue: ['(_PrimitiveFailureCode keyAtValue:  _failureCode)']
 					ifFalse: ['''failed''']).
-	text := text contents.
-	method := Compiler compile: text in: aBehavior.
-	self deny: method identicalTo: nil.
-	method sourceDescriptor: text.
-	^method!
+	^self compile: text contents in: aBehavior!
 
 createPrimitiveMethodLike: aCompiledMethod setsFailCode: aBoolean
 	^self
@@ -118,7 +121,11 @@ earlyTermination: aBoolean
 					count := count + 1].
 			"The curtailment block should now have run"
 			self assert: a identicalTo: 0.
-			"and the process should have died"
+			"and the process should also die fairly quickly after that"
+			count := 0.
+			[proc isTerminated not and: [count < 50]] whileTrue: 
+					[Processor sleep: 1 milliseconds.
+					count := count + 1].
 			self assert: proc isTerminated.
 			self assert: Processor pendingTerminations isEmpty]!
 
@@ -487,27 +494,26 @@ testExtCallRetHRESULT
 	"Test that HRESULT return codes are translated into exceptions correctly."
 
 	| method |
-	method := Compiler
+	method := self
 				compile: 'hresult: anInteger
-	<stdcall: hresult AnswerDWORD sdword>
-	| proc |
-	proc := Processor activeProcess.
-	^_failureCode -> proc primitiveFailureData'
+	<stdcall: hresult AnswerDWORD dword>
+	^self invalidCall: _failureCode'
 				in: VMLibrary.
 	#(0 1 16r3FFFFFFF 16r40000000 16r7FFFFFFF) do: 
 			[:each |
 			| result |
 			result := method value: VMLibrary default withArguments: {each}.
+			self assert: result isKindOf: Integer.
 			self assert: result equals: each.
 			self assert: each class identicalTo: result class].
-	"Negative HRESULTs should cause the primitive to fail with the failure data set to the HR code."
-	#(-1 -16r40000000 -16r40000001 -16r80000000) do: 
+
+	"Negative HRESULTs should cause the primitive to fail with the failure data set to the HR code. Note that the unused bit 27 is lost."
+	#(16rFFFFFFFF 16r80004005 16rC0000000 16rBFFFFFFF 16r80000000 16rEABCFFFF 16r80290422) do: 
 			[:each |
-			| result |
-			result := method value: VMLibrary default withArguments: {each}.
-			self assert: result key equals: -1.
-			self assert: result value equals: each.
-			self assert: each class identicalTo: result value class]!
+			self
+				should: [method value: VMLibrary default withArguments: {each}]
+				raise: HRESULTError
+				matching: [:ex | ex hresult = (HRESULT fromUnsignedInteger: (each bitAnd: 16r8000000 bitInvert))]]!
 
 testExtCallRetLpwstr
 	| expected actual method |
@@ -1060,6 +1066,31 @@ testMustBeBoolean
 	"Near jump if false"
 	self assert: (pseudoTrue ifTrue: [1 + 2 + 3 + 4] ifFalse: [2]) equals: 1 + 2 + 3 + 4.
 	self assert: (pseudoFalse ifTrue: [1 + 2 + 3 + 4] ifFalse: [2]) equals: 2!
+
+testOverlappedCallInvalidArguments
+	"Invalid arguments should be detected and handled as for a non-overlapped call."
+	self
+		should: [KernelLibrary default sleep: 's']
+		raise: InvalidExternalCall
+		matching: [:ex | ex argument = 's' and: [ex type = 'dword' and: [ex tag = 1]]].
+!
+
+testOverlappedCallRetHRESULT
+	"Test that HRESULT return codes are translated into exceptions correctly by overlapped calls."
+
+	| method |
+	method := self
+				compile: 'hr: anInteger <overlap stdcall: hresult AnswerDWORD sdword> ^self invalidCall: _failureCode'
+				in: VMLibrary.
+	{HRESULT win32Error: 1234. HRESULT fromUnsignedInteger: 16r80290422} do: 
+			[:each |
+			self
+				should: [method value: VMLibrary default withArguments: {each asInteger}]
+				raise: HRESULTError
+				matching: [:ex | ex hresult = each]].
+	"Positive codes are success values"
+	#(0 1 16r40290423)
+		do: [:each | self assert: (method value: VMLibrary default withArguments: {each}) equals: each]!
 
 testOverlappedCallsSuspendAndTerminate
 	5 timesRepeat: [self suspendAndTerminate]!
@@ -3002,6 +3033,7 @@ verifyPrimitiveNextPutAllOnUtfStrings: method
 !VMTest categoriesFor: #assertImmutableBasicAtPut:!private!unit tests! !
 !VMTest categoriesFor: #assertImmutableInstVarAtPut:!private!unit tests! !
 !VMTest categoriesFor: #assertImmutableResize:!private!unit tests! !
+!VMTest categoriesFor: #compile:in:!helpers!private! !
 !VMTest categoriesFor: #createPrimitiveMethod:in:argCount:setsFailCode:!helpers!private! !
 !VMTest categoriesFor: #createPrimitiveMethodLike:setsFailCode:!helpers!private! !
 !VMTest categoriesFor: #earlyTermination:!helpers!private! !
@@ -3045,6 +3077,8 @@ verifyPrimitiveNextPutAllOnUtfStrings: method
 !VMTest categoriesFor: #testIntDivideByZero!public!unit tests! !
 !VMTest categoriesFor: #testMakeLargeUnsigned!public!unit tests! !
 !VMTest categoriesFor: #testMustBeBoolean!public!unit tests! !
+!VMTest categoriesFor: #testOverlappedCallInvalidArguments!public!unit tests! !
+!VMTest categoriesFor: #testOverlappedCallRetHRESULT!public!unit tests! !
 !VMTest categoriesFor: #testOverlappedCallsSuspendAndTerminate!public!unit tests! !
 !VMTest categoriesFor: #testOverlappedCallTermination!public!unit tests! !
 !VMTest categoriesFor: #testPrimitiveAdd!public!unit tests! !

Core/DolphinVM/ExternalCall.asm
@@ -252,7 +252,6 @@ asyncDLL32Call PROC STDCALL PUBLIC USES edi esi ebx,
 performCall:
 	push	eax												; ARG1: cached proc address
 	call	callExternalFunction
-	mov		[STACKPOINTER], _SP		 						; Save down interpreter stack pointer, e.g. for C routine
 	ret														; eax will be non-zero as otherwise we'd not be here
 
 procAddressNotCached:
@@ -1760,56 +1759,23 @@ extCallRetHRESULT:
 
 	; FAILED(HRESULT)
 	ASSERTNEQU %RESULT, <ecx>
-	
+	ASSERTEQU %RESULT, <eax>
+
+	; Bit 27 is not used in a true HRESULT, so we can 
 	mov		ecx, RESULT
-	add		RESULT, RESULT					; Will it fit into a SmallInteger
-	jo		hrOverflow						; No, more than 31 bits required (will return to my caller)
-	or		eax, 1							; Yes, create SmallInteger to return in EAX
+	and		ecx, 7ffffffh
+	add		ecx, ecx
+	inc		ecx
+	and		RESULT, 0f0000000h
+	or		RESULT, ecx
 
 	mov		ecx, callContext
 	ASSUME	ecx:PTR InterpRegisters
-
-	mov		edx, [ecx].m_pActiveProcess
-	ASSUME	edx:PTR Process
 
 	mov		_SP, [ecx].m_stackPointer
 	mov		_IP, [ecx].m_instructionPointer
 	mov		_BP, [ecx].m_basePointer
 
-	mov		ecx, [edx].m_primitiveFailureData
-	mov		[edx].m_primitiveFailureData, eax
-	ASSUME	edx:NOTHING
-	
-	CountDownOopIn <c>
-
-	mov		eax, -1						; Fail the primitive
-	ret
-
-hrOverflow:
-	call	LINEWSIGNED		
-
-	mov		ecx, callContext
-	ASSUME	ecx:PTR InterpRegisters
-
-	; Result is a LargeInteger, so we must set ref. count to 1, but only because we are storing it into
-	; a non-stack slot in the process
-	ASSUME	eax:PTR OTE
-	mov		[eax].m_count, 1
-
-	mov		edx, [ecx].m_pActiveProcess
-	ASSUME	edx:PTR Process
-
-	mov		_SP, [ecx].m_stackPointer
-	mov		_IP, [ecx].m_instructionPointer
-	mov		_BP, [ecx].m_basePointer
-	ASSUME	ecx:NOTHING
-
-	mov		ecx, [edx].m_primitiveFailureData
-	mov		[edx].m_primitiveFailureData, eax
-	
-	CountDownOopIn <c>
-	
-	mov		eax, -1						; Fail the primitive
 	ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;

Core/DolphinVM/Interprt.h
@@ -281,6 +281,7 @@ public:
 
 	static bool disableAsyncGC(bool bDisable);
 	static void OnCompact();
+	static void CompactVirtualObject(OTE*);
 	static void MarkRoots();
 
 	// Clear down the object caches for VM alloc'd objects
@@ -478,9 +479,6 @@ public:
 	//		the argumentCount can be relied upon.
 
 	static Oop* primitiveFailure(_PrimitiveFailureCode failureCode);
-	static Oop* primitiveFailureWith(_PrimitiveFailureCode, Oop failureOop);
-	static Oop* primitiveFailureWith(_PrimitiveFailureCode failureCode, OTE* failureObject);
-	static Oop* primitiveFailureWithInt(_PrimitiveFailureCode failureCode, SMALLINTEGER failureInt);
 
 private:
 	// Answer whether an exception occurred in a primitive

Core/DolphinVM/InterprtPrim.inl
@@ -10,50 +10,7 @@
 
 #include "STProcess.h"	// In order to be able to set primitive failure code
 
-inline void ST::Process::SetPrimitiveFailureData(Oop failureData)
-{
-	ObjectMemory::countDown(m_primitiveFailureData);
-	ObjectMemory::countUp(failureData);
-	m_primitiveFailureData = failureData;
-}
-
-inline void ST::Process::SetPrimitiveFailureData(OTE* failureData)
-{
-	ObjectMemory::countDown(m_primitiveFailureData);
-	failureData->countUp();
-	m_primitiveFailureData = reinterpret_cast<Oop>(failureData);
-}
-
-inline void ST::Process::SetPrimitiveFailureData(SMALLINTEGER failureData)
-{
-	ObjectMemory::countDown(m_primitiveFailureData);
-	m_primitiveFailureData = integerObjectOf(failureData);
-}
-
 __forceinline Oop* Interpreter::primitiveFailure(_PrimitiveFailureCode failureCode)
 {
 	return reinterpret_cast<Oop*>(ObjectMemoryIntegerObjectOf(static_cast<int>(failureCode)));
-}
-
-inline Oop* Interpreter::primitiveFailureWith(_PrimitiveFailureCode failureCode, Oop failureData)
-{
-	Process* proc = actualActiveProcess();
-	proc->SetPrimitiveFailureData(failureData);
-	return primitiveFailure(failureCode);
-}
-
-inline Oop* Interpreter::primitiveFailureWith(_PrimitiveFailureCode failureCode, OTE* oteFailure)
-{
-	ASSERT(!ObjectMemoryIsIntegerObject(oteFailure));
-	Process* proc = actualActiveProcess();
-	proc->SetPrimitiveFailureData(oteFailure);
-	return primitiveFailure(failureCode);
-}
-
-// Just to avoid any confusion with Oop overload, give this one a different name
-inline Oop* Interpreter::primitiveFailureWithInt(_PrimitiveFailureCode failureCode, SMALLINTEGER failureData)
-{
-	Process* proc = actualActiveProcess();
-	proc->SetPrimitiveFailureData(failureData);
-	return primitiveFailure(failureCode);
 }

commit c2a33988f8f2a7407ee0a3d4bab14822fb3f298a

Fix premature GC of processes blocked in overlapped calls
Regression caused by 9ffb339942f86cc8a37b060dac0dc0e581f7d51e

Core/DolphinVM/interfac.cpp
@@ -428,6 +428,9 @@ void Interpreter::MarkRoots()
 		ObjectMemory::MarkObjectsAccessibleFromRoot(*m_roots[i]);
 		i++;
 	}
+
+
+	OverlappedCall::MarkRoots();
 }
 
 // A compacting GC has occurred, ask ObjectMemory to update any stored down Oops
@@ -454,21 +457,13 @@ void Interpreter::OnCompact()
 	ASSERT(ObjectMemory::isKindOf(m_registers.m_oteActiveProcess, Pointers.ClassProcess));
 
 	flushCaches();
+
+	OverlappedCall::OnCompact();
 }
 
 void Interpreter::CompactVirtualObject(OTE* ote)
 {
 	// TODO: Uncommit any virtual memory that is not currently in use by the object (i.e. shrink process stacks)
-
-	if (ote->m_oteClass == Pointers.ClassProcess)
-	{
-		ProcessOTE* oteProcess = reinterpret_cast<ProcessOTE*>(ote);
-		OverlappedCallPtr oc = oteProcess->m_location->GetOverlappedCall();
-		if (oc)
-		{
-			oc->OnCompact();
-		}
-	}
 }
 
 #pragma code_seg()

Core/DolphinVM/thrdcall.cpp
@@ -30,14 +30,17 @@ void __cdecl DebugDump(LPCWSTR szMsg);
 // Static data members
 ///////////////////////////////////////////////////////////////////////////////
 
+// Note that this static must only be accessed from the main interpreter thread
+// Its purpose is to maintain a list of all overlapped calls objects for admin
+// of same.
+OverlappedCallList OverlappedCall::s_activeList;
+
 // Time permitted for threads to complete before main thread gives up
 static DWORD s_dwTerminateTimeout;
 static bool bIsNT;
 
 const DWORD SE_VMTERMINATETHREAD = MAKE_CUST_SCODE(SEVERITY_ERROR, FACILITY_NULL, 0x300);
 
-//bool inPrim = false;
-//bool completed = false;
 
 inline void Process::NewOverlapSemaphore()
 {
@@ -85,7 +88,9 @@ OverlappedCallPtr OverlappedCall::New(ProcessOTE* oteProcess)
 {
 	HARDASSERT(::GetCurrentThreadId() == Interpreter::MainThreadId());
 
-	return new OverlappedCall(oteProcess);
+	OverlappedCall* answer = new OverlappedCall(oteProcess);
+	s_activeList.AddFirst(answer);
+	return answer;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -125,15 +130,23 @@ void OverlappedCall::TerminateThread()
 	#endif
 }
 
-//OverlappedCallPtr OverlappedCall::RemoveFirstFromList(OverlappedCallList& list)
-//{
-//	return list.RemoveFirst();
-//}
+OverlappedCallPtr OverlappedCall::RemoveFirstFromList(OverlappedCallList& list)
+{
+	return list.RemoveFirst();
+}
 
 // Static clean up async call support on shutdown
 void OverlappedCall::Uninitialize()
 {
 	HARDASSERT(::GetCurrentThreadId() == Interpreter::MainThreadId());
+
+	// Terminate all overlapped call threads
+	OverlappedCallPtr next = RemoveFirstFromList(s_activeList);
+	while (next)
+	{
+		next->TerminateThread();
+		next = RemoveFirstFromList(s_activeList);
+	}
 }
 
 
@@ -164,9 +177,37 @@ static HANDLE NewAutoResetEvent()
 void OverlappedCall::OnCompact()
 {
 	HARDASSERT(::GetCurrentThreadId() == Interpreter::MainThreadId());
-	HARDASSERT((POTE)m_oteProcess != Pointers.Nil);
 
+	// A compact is in progress, we need to ask the ObjectMemory to update
+	// our instances which might have stored down process Oops (i.e. any
+	// active or pending termination/completion).
+
+#if TRACING == 1
+	{
+		tracelock lock(::thinDump);
+		::thinDump << L"Compacting outstanding overlapped calls..." << std::endl;
+	}
+#endif
+	{
+		CompactCallsOnList(s_activeList);
+	}
+}
+
+void OverlappedCall::CompactCallsOnList(OverlappedCallList& list)
+{
+	OverlappedCall* next = list.First();
+	while (next)
+	{
+		next->compact();
+		next = next->Next();
+	}
+}
+
+void OverlappedCall::compact()
+{
+	HARDASSERT((POTE)m_oteProcess != Pointers.Nil);
 	ObjectMemory::compactOop(m_oteProcess);
+
 	#if TRACING == 1
 	{
 		TRACELOCK();
@@ -281,6 +322,7 @@ OverlappedCall::OverlappedCall(ProcessOTE* oteProcess) :
 	}
 	#endif
 
+	oteProcess->countUp();
 	Init();
 }
 
@@ -508,9 +550,6 @@ bool OverlappedCall::Initiate(CompiledMethod* pMethod, unsigned argCount)
 	}
 	#endif
 
-	// Note that the callDepth member examined by IsInCall() method is only  accessed 
-	// from the main thread, or when the main thread is blocked waiting for call 
-	// completion on the worker thread, therefore it needs no synchronisation
 	if (IsInCall())
 		// Nested overlapped calls are not currently supported
 		return false;
@@ -519,6 +558,7 @@ bool OverlappedCall::Initiate(CompiledMethod* pMethod, unsigned argCount)
 	m_pMethod = pMethod;
 	m_nArgCount = argCount;
 	ASSERT(m_oteProcess == Interpreter::actualActiveProcessPointer());
+
 	// Copy context from Interpreter
 	// ?? Not sure we'll need all this
 	m_interpContext = Interpreter::GetRegisters();
@@ -580,6 +620,8 @@ void OverlappedCall::OnActivateProcess()
 
 	if (CanComplete())
 	{
+		ASSERT(IsInCall());
+
 		// Let the overlapped thread continue
 		::SetEvent(m_hEvtGo);
 
@@ -895,7 +937,10 @@ void OverlappedCall::RemoveFromPendingTerminations()
 		HARDASSERT(oteProc->isNil());
 	}
 
-	m_oteProcess = (ProcessOTE*)Pointers.Nil;
+	NilOutPointer(m_oteProcess);
+
+	// Remove the call from the active list as it is being destroyed
+	Unlink();
 }
 
 // Let the interpreter know that this thread has completed the call, and is ready to finish
@@ -913,14 +958,14 @@ void OverlappedCall::NotifyInterpreterOfCallReturn()
 	#if TRACING == 1
 	{
 		TRACELOCK();
-		TRACESTREAM << std::hex << GetCurrentThreadId() << L": NotifyInterpreterOfCallReturn(), failure code = " << m_primitiveFailureCode << std::endl;
+		TRACESTREAM << std::hex << GetCurrentThreadId() << L": NotifyInterpreterOfCallReturn(), failure code = " << (SMALLINTEGER)m_primitiveFailureCode << std::endl;
 	}
 	#endif
 
 	InterlockedCompareExchange(reinterpret_cast<SHAREDLONG*>(&m_state), Returned, Calling);
 	Process* myProc = GetProcess();
 	Interpreter::asynchronousSignal(myProc->OverlapSemaphore());
-	//completed = true;
+
 	// We must set this event in case the main thread has quiesced
 	Interpreter::SetWakeupEvent();
 }
@@ -938,7 +983,7 @@ void OverlappedCall::WaitForInterpreter()
 		if (dwRet == WAIT_ABANDONED)
 		{
 			// Event deleted, etc, so terminate the thread
-			RaiseException(SE_VMTERMINATETHREAD, EXCEPTION_NONCONTINUABLE, 0, NULL);
+			::RaiseException(SE_VMTERMINATETHREAD, EXCEPTION_NONCONTINUABLE, 0, NULL);
 		}
 
 		// Interrupted to process an APC (probably a suspend/terminate) or due to a timeout
@@ -1014,6 +1059,34 @@ OverlappedCallPtr OverlappedCall::Do(CompiledMethod* pMethod, unsigned argCount)
 ///////////////////////////////////////////////////////////////////////////////
 // Interpreter primitive helpers
 
+void OverlappedCall::MarkRoots()
+{
+	HARDASSERT(::GetCurrentThreadId() == Interpreter::MainThreadId());
+
+	OverlappedCallPtr next = s_activeList.First();
+	while (next)
+	{
+		if (next->IsInCall())
+		{
+			ObjectMemory::MarkObjectsAccessibleFromRoot(reinterpret_cast<POTE>(next->m_oteProcess));
+		}
+		next = next->Next();
+	}
+}
+
+#ifdef _DEBUG
+void OverlappedCall::ReincrementProcessReferences()
+{
+	HARDASSERT(::GetCurrentThreadId() == Interpreter::MainThreadId());
+
+	OverlappedCallPtr next = s_activeList.First();
+	while (next)
+	{
+		next->m_oteProcess->countUp();
+		next = next->Next();
+	}
+}
+#endif
 ///////////////////////////////////////////////////////////////////////////////
 // Interpreter methods related to overlapped calls
 ///////////////////////////////////////////////////////////////////////////////

Core/DolphinVM/thrdcall.h
@@ -116,7 +116,75 @@ public:
 class OverlappedCall;
 typedef SmartPtr<OverlappedCall> OverlappedCallPtr;
 
-class OverlappedCall
+template <class T> class DoubleLink
+{
+	T* prev;
+	T* next;
+
+public:
+	DoubleLink() : prev(NULL), next(NULL) {}
+
+	T* Next() const { return next; }
+
+	T* Unlink()
+	{
+		// next null if last link in list
+		if (next)
+			next->prev = prev;
+		// prev pointer normally valid because there is an anchor node, but when shutting
+		// down will already have been unlinked from the list
+		if (prev)
+			prev->next = next;
+		next = prev = NULL;
+
+		return static_cast<T*>(this);
+	}
+
+	void LinkAfter(T* prevArg)
+	{
+		T* pThis = static_cast<T*>(this);
+		this->next = prevArg->next;
+		if (this->next)
+			this->next->prev = pThis;
+		this->prev = prevArg;
+		prevArg->next = pThis;
+	}
+};
+
+template <class T> class DoublyLinkedList
+{
+	DoubleLink<T> anchor;
+public:
+	T* First() const
+	{
+		return anchor.Next();
+	}
+
+	bool IsEmpty() const
+	{
+		return First() == NULL;
+	}
+
+	T* RemoveFirst()
+	{
+		T* node = First();
+		if (node != NULL)
+			node->Unlink();
+		return node;
+	}
+
+	void AddFirst(T* node)
+	{
+		// Slightly nasty thing is that anchor node is not actually the same type as
+		// the other nodes, so we need a cast here.
+		node->LinkAfter(reinterpret_cast<T*>(&anchor));
+	}
+};
+
+class OverlappedCall;
+typedef DoublyLinkedList<OverlappedCall> OverlappedCallList;
+
+class OverlappedCall : public DoubleLink<OverlappedCall>
 {
 public:
 	// Allocate/free methods for maintaining pool of available blocks
@@ -195,15 +263,21 @@ public:
 
 	static Semaphore* pendingTerms();
 
+	static void MarkRoots();
+	static void OnCompact();
+
 	void OnActivateProcess();
-	void OnCompact();
+
+#ifdef _DEBUG
+	static void ReincrementProcessReferences();
+#endif
 
 	bool IsInCall();
 
 private:
 	// Low-level management routines
 	static OverlappedCallPtr New(ProcessOTE*);
-	//static OverlappedCallPtr RemoveFirstFromList(OverlappedCallList&);
+	static OverlappedCallPtr RemoveFirstFromList(OverlappedCallList&);
 
 	// Thread entry point function
 	static unsigned __stdcall ThreadMain(void* pThis);
@@ -215,13 +289,14 @@ private:
 	static OverlappedCallPtr BeginAPC(DWORD dwParam);
 	static OverlappedCallPtr BeginMainThreadAPC(DWORD dwParam);
 
-	//static void CompactCallsOnList(OverlappedCallList& list);
+	static void CompactCallsOnList(OverlappedCallList& list);
+	void compact();
 
 	///////////////////////////////////////////////////////////////////////////
 	// Static data members
 private:
 
-	//static OverlappedCallList s_activeList;
+	static OverlappedCallList s_activeList;
 	
 	static bool s_bShutdown;
 

Core/DolphinVM/vmref.cpp
@@ -119,6 +119,8 @@ void Interpreter::RemoveVMReference(OTE* pOTE)
 		m_oteNewProcess->countUp();					// new process or nil
 		m_oteUnderConstruction->countUp();			// Window currently under construction or nil
 		
+		OverlappedCall::ReincrementProcessReferences();
+
 		ObjectMemory::addVMRefs();
 	}
 #endif

Core/Object Arts/Dolphin/IDE/Base/VMTest.cls
@@ -1077,6 +1077,34 @@ testMustBeBoolean
 	self assert: (pseudoTrue ifTrue: [1 + 2 + 3 + 4] ifFalse: [2]) equals: 1 + 2 + 3 + 4.
 	self assert: (pseudoFalse ifTrue: [1 + 2 + 3 + 4] ifFalse: [2]) equals: 2!
 
+testOverlappedCallGC
+	| kernel event finished delay1 delay2 |
+	kernel := KernelLibrary default.
+	event := kernel
+				createEvent: nil
+				bManualReset: false
+				bInitialState: false
+				lpName: nil.
+	finished := false.
+	delay1 := Delay forDuration: 50 milliseconds.
+	delay2 := Delay forDuration: 20 milliseconds.
+	
+	[delay1 signal.
+	kernel waitForSingleObject: event dwMilliseconds: -1.
+	finished := true.
+	delay2 signal]
+			forkAt: Processor userInterruptPriority.
+	self deny: finished.
+	delay1 wait.
+	1 timesRepeat: 
+			[MemoryManager current
+				collectGarbage;
+				administerLastRites].
+	self deny: finished.
+	kernel setEvent: event.
+	delay2 wait.
+	self assert: finished!
+
 testOverlappedCallInvalidArguments
 	"Invalid arguments should be detected and handled as for a non-overlapped call."
 	self
@@ -3166,6 +3194,7 @@ verifyPrimitiveNextPutAllOnUtfStrings: method
 !VMTest categoriesFor: #testIntDivideByZero!public!unit tests! !
 !VMTest categoriesFor: #testMakeLargeUnsigned!public!unit tests! !
 !VMTest categoriesFor: #testMustBeBoolean!public!unit tests! !
+!VMTest categoriesFor: #testOverlappedCallGC!public!unit tests! !
 !VMTest categoriesFor: #testOverlappedCallInvalidArguments!public!unit tests! !
 !VMTest categoriesFor: #testOverlappedCallRetHRESULT!public!unit tests! !
 !VMTest categoriesFor: #testOverlappedCallsSuspendAndTerminate!public!unit tests! !