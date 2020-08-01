dolphinsmalltalk/Dolphin

commit 978b3bf30648cb623a1dfc982ce9be0860455cb0

Store only the get selector in ExternalStructure templates

Core/Object Arts/Dolphin/Base/ExternalStructure.cls
@@ -185,13 +185,18 @@ doesNotUnderstand: aMessage
 	selector of aMessage is looked up in the receiver's class' template, and 
 	if found the appropriate field is retrieved/set."
 
-	| args field |
-	args := aMessage arguments.
-	field := self template at: aMessage selector ifAbsent: [^super doesNotUnderstand: aMessage].
-	args size == 0
-		ifTrue: [field isReadable ifTrue: [^field readFrom: self]]
-		ifFalse: [field isWriteable ifTrue: [^field writeInto: self value: (args at: 1)]].
-	^super doesNotUnderstand: aMessage!
+	| key |
+	(key := aMessage selector) last == $: ifTrue: [key := key copyFrom: 1 to: key size - 1].
+	^(self template lookup: key)
+		ifNil: [super doesNotUnderstand: aMessage]
+		ifNotNil: 
+			[:field |
+			| args |
+			args := aMessage arguments.
+			args size == 0
+				ifTrue: [field isReadable ifTrue: [^field readFrom: self]]
+				ifFalse: [field isWriteable ifTrue: [^field writeInto: self value: (args at: 1)]].
+			super doesNotUnderstand: aMessage]!
 
 fieldsDo: aMonadicValuable
 	"Evaluate the <monadicValuable> argument, operation, for each of the receiver's fields."
@@ -529,7 +534,6 @@ defineField: fieldName type: anExternalField offset: offset
 	"We always retain the read selector in the template - this is needed to calc the field
 	offset when compiling methods, and might be required for evaluating size_is expressions."
 	template at: fieldName asSymbol put: anExternalField.
-	anExternalField isWriteable ifTrue: [template at: (fieldName , ':') asSymbol put: anExternalField].
 	"Define a class constant for the field offset, if needed"
 	offsetConstant := self fieldOffsetConstantPrefix , fieldName.
 	(self bindingFor: offsetConstant) ifNotNil: [:var | var value == offset ifTrue: [^self]].
@@ -746,7 +750,7 @@ newTemplate
 	"Private - Reset the template instance variable of the receiver in preparation for loading a
 	new template."
 
-	template := IdentityDictionary new.
+	template := LookupTable new.
 	alignment := self baseAlignment!
 
 offsetFor: anExternalField base: anInteger 

commit 49b5219c3fd17266ab6df89a9e0c1e392244526f

ExternalStructure class>>dynamicSelectors excludes mutators
Regression caused by 978b3bf30648cb623a1dfc982ce9be0860455cb0

Core/Object Arts/Dolphin/Base/ExternalStructure.cls
@@ -580,7 +580,10 @@ descriptorClass
 dynamicSelectorsDo: aMonadicValuable
 	"Evaluate the <monadicValuable> argument for each of the extra message selectors understood by the receiver at run time that might not have an associated method."
 
-	self template keysDo: aMonadicValuable!
+	self template keysAndValuesDo: 
+			[:fieldName :field |
+			field isReadable ifTrue: [aMonadicValuable value: fieldName].
+			field isWriteable ifTrue: [aMonadicValuable value: (fieldName copyWith: $:) asSymbol]]!
 
 elementSize
 	"Answer the size (in bytes) of the structure the receiver represents

Core/Object Arts/Dolphin/Base/ExternalStructureTest.cls
@@ -30,6 +30,18 @@ testCompileDefinition
 	self assert: subject selectors asArray sort equals: #(#a #b: #c #c:).
 	self assert: subject byteSize equals: 12!
 
+testDynamicSelectors
+	| subject expected |
+	subject := ExternalStructure newAnonymousSubclass.
+	subject initializeTemplate.
+	subject
+		defineField: #writeOnly type: DWORDField writeOnly;
+		defineField: #readOnly type: DWORDField readOnly;
+		defineField: #readWrite type: DWORDField new;
+		defineField: #fillter type: INT_PTRField filler.
+	expected := #(#readOnly #readWrite #readWrite: #writeOnly:).
+	self assert: subject dynamicSelectors asArray sort equals: expected!
+
 testEquals
 	| s1 s2 s3 s4 |
 	s1 := FILETIME newPointer.
@@ -92,6 +104,7 @@ testIsNull
 	struct bytes: ExternalAddress new.
 	self assert: struct isNull.! !
 !ExternalStructureTest categoriesFor: #testCompileDefinition!public!unit tests! !
+!ExternalStructureTest categoriesFor: #testDynamicSelectors!public!unit tests! !
 !ExternalStructureTest categoriesFor: #testEquals!public!unit tests! !
 !ExternalStructureTest categoriesFor: #testExtendedErrorInfo!public!unit tests! !
 !ExternalStructureTest categoriesFor: #testIsNull!public!unit tests! !