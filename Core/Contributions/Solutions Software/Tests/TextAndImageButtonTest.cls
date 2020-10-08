dolphinsmalltalk/Dolphin

commit ccbbf10bbc5e7fa608688879431fdc2f8a888280

Add test of drawing button text with an ampersand

Core/Contributions/Solutions Software/Tests/TextAndImageButtonTest.cls
@@ -1,0 +1,57 @@
+﻿"Filed out from Dolphin Smalltalk 7"!
+
+TestCase subclass: #TextAndImageButtonTest
+	instanceVariableNames: 'button canvas anon'
+	classVariableNames: ''
+	poolDictionaries: ''
+	classInstanceVariableNames: ''!
+TextAndImageButtonTest guid: (GUID fromString: '{fdb7cc36-fd20-49e9-b512-b324a64641e3}')!
+TextAndImageButtonTest comment: ''!
+!TextAndImageButtonTest categoriesForClass!SUnit! !
+!TextAndImageButtonTest methodsFor!
+
+setUp
+	super setUp.
+	button := TextAndImageButton new.
+	ShellView show addSubView: button!
+
+tearDown
+	button topView destroy.
+	button := nil.
+	super tearDown!
+
+testLabelWithAmpersand
+	"Test that labels containing ampersands are not (normally) ellipsised.
+	Inspired by TabViewXPTest>>testLabelWithAmpersand"
+
+	| extent text |
+	text := '&Hello && World'.
+	button text: text.
+	anon := Canvas newAnonymousSubclass.
+	anon basicClassPool: (anon classPool
+				at: 'DrawnText' put: nil;
+				at: 'Rect' put: nil;
+				yourself).
+	canvas := button canvas.
+	canvas becomeAn: anon.
+	canvas font: button actualFont.
+	anon
+		basicCompile: 'formatText: aString in: aRectangle flags: anInteger
+		"Double the size to avoid any chance of buffer overrun"
+		DrawnText := aString asUtf16String, (Utf16String new: aString size).
+		Rect := aRectangle.
+		^super formatText: DrawnText in: aRectangle flags: anInteger | DT_MODIFYSTRING'.
+	extent := canvas
+				textExtent: text
+				width: 0
+				flags: Win32Constants.DT_SINGLELINE.
+	button drawTextOn: canvas.
+	canvas free.
+	"The text should not have been ellipsized, and the drawn extent should be
+	the full extent needed to draw the text on a single line."
+	self assert: (anon classPool at: 'DrawnText') trimNulls equals: text.
+	self assert: (anon classPool at: 'Rect') extent = extent! !
+!TextAndImageButtonTest categoriesFor: #setUp!public!running! !
+!TextAndImageButtonTest categoriesFor: #tearDown!public!running! !
+!TextAndImageButtonTest categoriesFor: #testLabelWithAmpersand!public!unit tests! !
+

Core/Contributions/Solutions Software/Tests/SSW Widget Enhancements Tests.pax
@@ -1,0 +1,38 @@
+﻿| package |
+package := Package name: 'SSW Widget Enhancements Tests'.
+package paxVersion: 1;
+	basicComment: ''.
+
+
+package classNames
+	add: #TextAndImageButtonTest;
+	yourself.
+
+package binaryGlobalNames: (Set new
+	yourself).
+
+package globalAliases: (Set new
+	yourself).
+
+package setPrerequisites: #(
+	'..\..\..\Object Arts\Dolphin\MVP\Base\Dolphin MVP Base'
+	'..\SSW Widget Enhancements'
+	'..\..\Camp Smalltalk\SUnit\SUnit').
+
+package!
+
+"Class Definitions"!
+
+TestCase subclass: #TextAndImageButtonTest
+	instanceVariableNames: 'button canvas anon'
+	classVariableNames: ''
+	poolDictionaries: ''
+	classInstanceVariableNames: ''!
+
+"Global Aliases"!
+
+
+"Loose Methods"!
+
+"End of package definition"!
+

RegressionTestsLoad.st
@@ -9,6 +9,7 @@ Package manager
 	install: 'Core\Contributions\Camp Smalltalk\ANSI\Tests\CS ANSI Tests.pax';
 	install: 'Core\Contributions\IDB\Tests\IDB Method History Tests.pax';
 	install: 'Core\Contributions\Solutions Software\Tests\SSW EditableListView Tests.pax';
+	install: 'Core\Contributions\Solutions Software\Tests\SSW Widget Enhancements Tests.pax';
 	install: 'Core\Object Arts\Dolphin\System\Base64\Dolphin Base64 Tests.pax';
 	install: 'Core\Object Arts\Dolphin\System\Filer\Dolphin Literal Filer Tests.pax';
 	install: 'Core\Object Arts\Dolphin\System\Random\Dolphin Random Stream Tests.pax';

commit d8cf1a5b8bf3fed2772216ccb950d09c2071a06e

Fix #1032: TextAndImageButton displayed incorrectly
Fix regression caused by PR #1028, and add regression coverage.
The existing test is extended because tests that create views have a high
overhead to run, and it is related.

Core/Contributions/Solutions Software/TextAndImageButton.cls
@@ -266,7 +266,8 @@ textExtentOn: aCanvas
 textPosOn: aCanvas
 	| xPos yPos |
 	xPos := self contentOffsetOn: aCanvas.
-	(self hasImage and: [self isImageFirst]) ifTrue: [xPos := self imageExtent x + self imageGap].
+	(self hasImage and: [self isImageFirst])
+		ifTrue: [xPos := xPos + self imageExtent x + self imageGap].
 	yPos := (self extent y - (self textExtentOn: aCanvas) y) // 2.
 	^self clientRectangle topLeft + (xPos @ yPos max: 0 @ 0)!
 

Core/Contributions/Solutions Software/Tests/TextAndImageButtonTest.cls
@@ -11,9 +11,18 @@ TextAndImageButtonTest comment: ''!
 !TextAndImageButtonTest methodsFor!
 
 setUp
+	| shell |
 	super setUp.
 	button := TextAndImageButton new.
-	ShellView show addSubView: button!
+	button font: (Font name: 'Segoe UI' pointSize: 8).
+	button image: Icon warning.
+	button hasBorder: true.
+	shell := ShellView new create.
+	shell layoutManager: BorderLayout new.
+	shell addSubView: button.
+	button arrangement: #center.
+	shell rectangle: (100 @ 100 extent: 250 @ 100).
+	shell show!
 
 tearDown
 	button topView destroy.
@@ -24,7 +33,7 @@ testLabelWithAmpersand
 	"Test that labels containing ampersands are not (normally) ellipsised.
 	Inspired by TabViewXPTest>>testLabelWithAmpersand"
 
-	| extent text |
+	| extent text textRect imageRect textPos |
 	text := '&Hello && World'.
 	button text: text.
 	anon := Canvas newAnonymousSubclass.
@@ -45,12 +54,19 @@ testLabelWithAmpersand
 				textExtent: text
 				width: 0
 				flags: Win32Constants.DT_SINGLELINE.
-	button drawTextOn: canvas.
-	canvas free.
+	"The test button needs to be large enough"
+	self assert: button extent > (extent + button imageExtent + (20@10)).
+	button drawOn: canvas.
 	"The text should not have been ellipsized, and the drawn extent should be
 	the full extent needed to draw the text on a single line."
 	self assert: (anon classPool at: 'DrawnText') trimNulls equals: text.
-	self assert: (anon classPool at: 'Rect') extent = extent! !
+	self assert: (anon classPool at: 'Rect') extent = extent.
+	"The text should not occlude the image"
+	imageRect := (button imagePosOn: canvas) extent: button imageExtent.
+	textRect := (button textPosOn: canvas) extent: extent.
+	self deny: (imageRect intersects: textRect).
+	canvas free.
+! !
 !TextAndImageButtonTest categoriesFor: #setUp!public!running! !
 !TextAndImageButtonTest categoriesFor: #tearDown!public!running! !
 !TextAndImageButtonTest categoriesFor: #testLabelWithAmpersand!public!unit tests! !