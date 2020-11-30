dolphinsmalltalk/Dolphin

commit 3b51dabd511bd1b027cd37876584511bffb4c8f6

Small optimisations to reduce PackageClosureTests run times

Core/Object Arts/Dolphin/Base/MethodCategory.cls
@@ -59,10 +59,21 @@ contents
 	Notification deprecated.
 	^self methods!
 
-includesMethod: method
-	"Answer whether the receiver includes the <CompiledMethod>, method."
+includesBehavior: aBehavior
+	"Answer whether the receiver categorizes any methods in the specified class."
 
-	^(method methodClass selectorsInCategory: self) identityIncludes: method selector!
+	^aBehavior includesCategory: self!
+
+includesMethod: aCompiledMethod
+	"Answer whether the receiver includes the <CompiledMethod> argument."
+
+	^(aCompiledMethod methodClass selectorsInCategory: self) identityIncludes: aCompiledMethod selector!
+
+isEmpty
+	"Answer whether the receiver is an empty category (with no members)"
+
+	self class environment allBehaviorsDo: [:each | self methodsInBehavior: each do: [:method | ^false]].
+	^true!
 
 isPrivacy
 	"Private - Answer whether the receiver is a method privacy category."
@@ -74,17 +85,16 @@ methods
 
 	| foundMethods |
 	foundMethods := IdentitySet new.
-	self behaviorsDo: [:aBehavior | foundMethods addAll: (self methodsInBehavior: aBehavior)].
+	self class environment
+		allBehaviorsDo: [:each | self methodsInBehavior: each do: [:method | foundMethods add: method]].
 	^foundMethods!
 
-methodsInBehavior: aBehavior 
+methodsInBehavior: aBehavior
 	"Answer a <sequencedReadableCollection> of <CompiledMethods>s being all the methods in the <Behavior>
 	argument which are members of the receiver."
 
-	| answer |
-	answer := OrderedCollection new: 32.
-	self methodsInBehavior: aBehavior do: [:each | answer addLast: each].
-	^answer!
+	^(aBehavior selectorsInCategory: self)
+		collect: [:eachSelector | aBehavior compiledMethodAt: eachSelector]!
 
 methodsInBehavior: aBehavior do: aMonadicValuable 
 	(aBehavior selectorsInCategory: self) do: 
@@ -117,7 +127,12 @@ removeMethodSilently: method
 	"Private - Remove the <CompiledMethod>, method, from the set of 
 	methods that are members of the receiver, and make no fuss about it." 
 
-	method methodClass removeSelector: method selector fromCategory: self! !
+	method methodClass removeSelector: method selector fromCategory: self!
+
+selectorsInBehavior: aBehavior
+	"Answer a <sequencedReadableCollection> of <Symbol>s being the selectors of methods in the <Behavior> argument which are members of the receiver."
+
+	^aBehavior selectorsInCategory: self! !
 !MethodCategory categoriesFor: #addClass:!adding!public! !
 !MethodCategory categoriesFor: #addMethod:!adding!public! !
 !MethodCategory categoriesFor: #addMethods:!adding!public! !
@@ -126,15 +141,18 @@ removeMethodSilently: method
 !MethodCategory categoriesFor: #behaviors!enumerating!private! !
 !MethodCategory categoriesFor: #behaviorsDo:!enumerating!public! !
 !MethodCategory categoriesFor: #contents!enumerating!public! !
+!MethodCategory categoriesFor: #includesBehavior:!enumerating!public! !
 !MethodCategory categoriesFor: #includesMethod:!public!testing! !
+!MethodCategory categoriesFor: #isEmpty!public!testing! !
 !MethodCategory categoriesFor: #isPrivacy!private!testing! !
-!MethodCategory categoriesFor: #methods!public! !
+!MethodCategory categoriesFor: #methods!enumerating!public! !
 !MethodCategory categoriesFor: #methodsInBehavior:!enumerating!public! !
 !MethodCategory categoriesFor: #methodsInBehavior:do:!enumerating!public! !
 !MethodCategory categoriesFor: #removeClass:!public!removing! !
 !MethodCategory categoriesFor: #removeMethod:!public!removing! !
 !MethodCategory categoriesFor: #removeMethods:!public!removing! !
 !MethodCategory categoriesFor: #removeMethodSilently:!private!removing! !
+!MethodCategory categoriesFor: #selectorsInBehavior:!enumerating!public! !
 
 !MethodCategory class methodsFor!
 

Core/Contributions/Refactory/Refactoring Browser/Environments/BrowserEnvironment.cls
@@ -186,6 +186,15 @@ forMethods: methodCollection
 		label: ('Some methods in <1d>' expandMacrosWith: self);
 		yourself!
 
+forMethodsIn: aBrowserEnvironment
+	| env |
+	env := (SelectorEnvironment onEnvironment: self)
+				label: ('Some methods in <1d>' expandMacrosWith: self);
+				yourself.
+	aBrowserEnvironment
+		classesAndSelectorsDo: [:class :selector | env addClass: class selector: selector].
+	^env!
+
 implementorsMatching: aString 
 	^SelectorEnvironment implementorsMatching: aString in: self!
 
@@ -247,14 +256,12 @@ isSystem
 
 keys
 	| keys |
-	keys := Set new.
+	keys := IdentitySet new.
 	Smalltalk keysAndValuesDo: 
-			[:key :value | 
-			| class |
-			value isBehavior 
-				ifTrue: [(self includesClass: value) ifTrue: [keys add: key]].
-			class := value class.
-			(self includesClass: class) ifTrue: [keys add: key]].
+			[:key :value |
+			value isBehavior
+				ifTrue: [((self includesClass: value) or: [self includesClass: value class]) ifTrue: [keys add: key]]
+				ifFalse: [(self includesClass: value class) ifTrue: [keys add: key]]].
 	^keys!
 
 label
@@ -288,9 +295,14 @@ numberClasses
 	^self classNames size!
 
 numberSelectors
-	| total |
+	| total classes |
 	total := 0.
-	Smalltalk allBehaviorsDo: [:class | total := total + class numberSelectors].
+	classes := Smalltalk allClasses.
+	1 to: classes size
+		do: 
+			[:i |
+			| class |
+			total := total + (class := classes at: i) numberSelectors + class class numberSelectors].
 	^total!
 
 printOn: aStream
@@ -394,6 +406,7 @@ whichProtocolsInclude: aSelector in: aClass
 !BrowserEnvironment categoriesFor: #forClasses:!environments!public! !
 !BrowserEnvironment categoriesFor: #forClassHierarchyOf:!environments!public! !
 !BrowserEnvironment categoriesFor: #forMethods:!environments!public! !
+!BrowserEnvironment categoriesFor: #forMethodsIn:!environments!public! !
 !BrowserEnvironment categoriesFor: #implementorsMatching:!environments!public! !
 !BrowserEnvironment categoriesFor: #implementorsOf:!environments!public! !
 !BrowserEnvironment categoriesFor: #includesCategory:!public!testing! !

Core/Contributions/Refactory/Refactoring Browser/Environments/BrowserEnvironmentWrapper.cls
@@ -24,29 +24,30 @@ allClasses
 allClassesDo: aBlock
 	"Private - Evaluate the <monadicValuable> argument, aBlock, for every class that could possibly be included in the receiver."
 
-	| allClasses numClasses step |
+	| allClasses |
 	allClasses := Smalltalk allClasses.
-	numClasses := allClasses size.
 	progressBlock
 		ifNil: 
-			[1 to: numClasses
+			[1 to: allClasses size
 				do: 
 					[:i |
 					| class |
-					class := allClasses at: i.
-					aBlock value: class.
-					aBlock value: class class]]
+					aBlock
+						value: (class := allClasses at: i);
+						value: class class]]
 		ifNotNil: 
 			[:progress |
+			| numClasses step |
+			numClasses := allClasses size.
 			step := numClasses // 100.
 			progress value: 0.
 			1 to: numClasses
 				do: 
 					[:i |
 					| class |
-					class := allClasses at: i.
-					aBlock value: class.
-					aBlock value: class class.
+					aBlock
+						value: (class := allClasses at: i);
+						value: class class.
 					i \\ step == 0 ifTrue: [progress value: i * 100 // numClasses]].
 			progress value: 100]!
 

Core/Contributions/Refactory/Refactoring Browser/Environments/CategoryEnvironment.cls
@@ -17,7 +17,7 @@ categories
 	^categories select: [:each | self includesCategory: each]!
 
 categories: aCollection
-	categories := aCollection!
+	categories := aCollection asSet!
 
 classNames
 	^self categories inject: IdentitySet new

Core/Contributions/Refactory/Refactoring Browser/Environments/RBEnvironments.pax
@@ -151,7 +151,7 @@ isBehavior
 numberSelectors
 	"Answer the number of selectors in the receiver's method dictionary."
 
-	^(methodDictionary ?? #()) size!
+	^methodDictionary ifNil: [0] ifNotNil: [methodDictionary size]!
 
 selectorsAndMethodsDo: aDyadicValuable
 	"Evaluate the <dyadicValuable> argument for each of the selector/method pairs in 

Core/Contributions/Refactory/Refactoring Browser/Tests/BrowserEnvironmentTest.cls
@@ -44,7 +44,7 @@ copyFor: aBrowserEnvironment
 
 keysFor: aBrowserEnvironment 
 	| allKeys |
-	allKeys := Set withAll: aBrowserEnvironment keys.
+	allKeys := aBrowserEnvironment keys asSet.
 	allKeys addAll: aBrowserEnvironment not keys.
 	allKeys removeAll: Smalltalk keys.
 	self assert: allKeys isEmpty!

Core/Contributions/Refactory/Refactoring Browser/Tests/RBTests.pax
@@ -379,7 +379,7 @@ isEqualTo: anObject
 
 !CompiledCode methodsFor!
 
-isEqualTo: aMethod 
+isEqualTo: aMethod
 	| count |
 	self == aMethod ifTrue: [^true].
 	self class = aMethod class ifFalse: [^false].
@@ -389,13 +389,13 @@ isEqualTo: aMethod
 	"We need to special case static blocks in the literal frame to avoid infinite recursion"
 	1 to: count
 		do: 
-			[:i | 
+			[:i |
 			| a b |
-			a := self at: i.
-			b := aMethod at: i.
+			a := self basicAt: i.
+			b := aMethod basicAt: i.
 			"The static blocks must be equal if they are implemented by this method and
 			the bytecodes and all other literals are identical"
-			(a class == BlockClosure and: [a method == self]) 
+			(a class == BlockClosure and: [a method == self])
 				ifTrue: [(b class == BlockClosure and: [b method == aMethod]) ifFalse: [^false]]
 				ifFalse: [(a isEqualTo: b) ifFalse: [^false]]].
 	^true! !

Core/Object Arts/Dolphin/Base/AbstractDictionaryTest.cls
@@ -47,6 +47,22 @@ newDictionary
 newEmpty
 	^self collectionClass new!
 
+testAssociationsDo
+	| subject actual expected |
+	subject := self newEmpty.
+	subject associationsDo: [:assoc | self fail: 'empty collection should not be enumerated'].
+	expected := Set new.
+	(1 to: 5) do: 
+			[:each |
+			| key value |
+			key := self makeKey: each.
+			value := self assimilate: each.
+			expected add: (subject associationClass key: key value: value).
+			subject at: key put: value].
+	actual := Set new.
+	subject associationsDo: [:each | actual add: each].
+	self assert: actual equals: expected!
+
 testAtIfAbsent
 	| dictionary |
 	dictionary := self newDictionary.
@@ -238,6 +254,7 @@ testStrongTalkTests
 !AbstractDictionaryTest categoriesFor: #newCollection:!helpers!private! !
 !AbstractDictionaryTest categoriesFor: #newDictionary!private!unit tests! !
 !AbstractDictionaryTest categoriesFor: #newEmpty!helpers!public! !
+!AbstractDictionaryTest categoriesFor: #testAssociationsDo!public!unit tests! !
 !AbstractDictionaryTest categoriesFor: #testAtIfAbsent!public!unit tests! !
 !AbstractDictionaryTest categoriesFor: #testAtIfAbsentPut!public!unit tests! !
 !AbstractDictionaryTest categoriesFor: #testAtIfAbsentPutModifyingCollection!public!unit tests! !

commit 75bd8551aed57c741e0ca7a943ebeae3a2a3da54

Fix #1061: ImageStripper attempting to remove already-removed method
This regression was caused by 3b51dabd511bd1b027cd37876584511bffb4c8f6,
escaping detection due to absence of test coverage.

Core/Object Arts/Dolphin/Base/Dolphin Base Tests.pax
@@ -78,6 +78,7 @@ package classNames
 	add: #MenuPainterTest;
 	add: #MergesortAlgorithmTest;
 	add: #MetaclassTest;
+	add: #MethodCategoryTest;
 	add: #MethodDictionaryTest;
 	add: #MustBeBooleanTestClasses;
 	add: #NeverSearchPolicyTest;
@@ -356,6 +357,11 @@ DolphinTest subclass: #MemoryManagerTest
 	poolDictionaries: ''
 	classInstanceVariableNames: ''!
 DolphinTest subclass: #MenuPainterTest
+	instanceVariableNames: ''
+	classVariableNames: ''
+	poolDictionaries: ''
+	classInstanceVariableNames: ''!
+DolphinTest subclass: #MethodCategoryTest
 	instanceVariableNames: ''
 	classVariableNames: ''
 	poolDictionaries: ''

Core/Object Arts/Dolphin/Base/MethodCategory.cls
@@ -90,11 +90,19 @@ methods
 	^foundMethods!
 
 methodsInBehavior: aBehavior
-	"Answer a <sequencedReadableCollection> of <CompiledMethods>s being all the methods in the <Behavior>
-	argument which are members of the receiver."
+	"Answer a <sequencedReadableCollection> of <CompiledMethods>s being all the methods in the <Behavior> argument which are members of the receiver."
 
-	^(aBehavior selectorsInCategory: self)
-		collect: [:eachSelector | aBehavior compiledMethodAt: eachSelector]!
+	| methods selectors count |
+	selectors := aBehavior selectorsInCategory: self.
+	"This needs to be robust to the method catalogue being out of date with respect to the actual method dictionary, which can happen if something has removed methods directly from the MethodDictionary such that no system update events were triggered."
+	count := selectors size.
+	methods := Array writeStream: count.
+	1 to: count
+		do: 
+			[:i |
+			(aBehavior compiledMethodAt: (selectors at: i) ifAbsent: [])
+				ifNotNil: [:each | methods nextPut: each]].
+	^methods grabContents!
 
 methodsInBehavior: aBehavior do: aMonadicValuable 
 	(aBehavior selectorsInCategory: self) do: 

Core/Object Arts/Dolphin/Lagoon/BasicImageStripper.cls
@@ -308,8 +308,7 @@ prepareExternalStructures
 	structs do: 
 			[:each |
 			"Access the byteSize to ensure set (important if not compiling external structures)"
-			each byteSize.
-			each class methodDictionary removeKey: #defineFields ifAbsent: []].
+			each byteSize].
 	"Remove the structure template definition and compiling mechanisms"
 	#('compiling' 'template definition') do: 
 			[:each |
@@ -317,6 +316,8 @@ prepareExternalStructures
 				removeMethods: (each asMethodCategory methodsInBehavior: ExternalStructure class)
 				reason: 'structure definition: ' , each
 				force: true].
+	"Remove any remaining defineFields methods that might have not been correctly categorised"
+	structs do: [:each | self removeSelectors: #(#defineFields) of: each class].
 	self endElement: true.
 	self
 		finishedWithAll: #(#compileExternalStructures #compileExternalStructuresNow: #defineExternalStructures: #prepareExternalStructures #externalStructureClasses)!

Core/Object Arts/Dolphin/Base/MethodCategoryTest.cls
@@ -1,0 +1,25 @@
+﻿"Filed out from Dolphin Smalltalk 7"!
+
+DolphinTest subclass: #MethodCategoryTest
+	instanceVariableNames: ''
+	classVariableNames: ''
+	poolDictionaries: ''
+	classInstanceVariableNames: ''!
+MethodCategoryTest guid: (GUID fromString: '{bf38215d-fed2-4dc6-9f36-92956bc1b19d}')!
+MethodCategoryTest comment: ''!
+!MethodCategoryTest categoriesForClass!Unclassified! !
+!MethodCategoryTest methodsFor!
+
+testMethodsInBehavior
+	| subject testBehavior methods keyMethod |
+	subject := 'accessing' asMethodCategory.
+	methods := subject methodsInBehavior: Association.
+	keyMethod := Association compiledMethodAt: #key.
+	self assert: (methods includes: keyMethod).
+	testBehavior := Association newAnonymousSubclass.
+	testBehavior methodsCatalogue: Association methodsCatalogue copy.
+	testBehavior methodDictionary: (MethodDictionary with: #key -> keyMethod).
+	methods := subject methodsInBehavior: testBehavior.
+	self assert: methods asArray equals: {keyMethod}! !
+!MethodCategoryTest categoriesFor: #testMethodsInBehavior!public! !
+