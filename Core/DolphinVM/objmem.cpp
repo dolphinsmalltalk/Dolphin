b47c515b96f0f498be2c5be0e3e7a2e264d346d9
James Foster <git@jgfoster.net>
2022-12-31 20:24:34 -0800

    Fix build break caused by deprecated of stdext::hash_value in VS 17.3

--- Core/DolphinVM/objmem.cpp
+++ Core/DolphinVM/objmem.cpp
@@ -271,9 +271,21 @@ bool __stdcall ObjectMemory::inheritsFrom(const BehaviorOTE* behaviorPointer, co
 
 #pragma code_seg(GC_SEG)
 
+namespace stdext {
+	template <class _Kty>
+	_NODISCARD size_t hash_value(const _Kty& _Keyval) noexcept {
+		if constexpr (_STD is_pointer_v<_Kty> || _STD is_null_pointer_v<_Kty>) {
+			return reinterpret_cast<size_t>(_Keyval) ^ 0xdeadbeefu;
+		}
+		else {
+			return static_cast<size_t>(_Keyval) ^ 0xdeadbeefu;
+		}
+	}
+};
+
 template <class T> inline size_t hash_value(const TOTE<T>* ote)
 {
-	return std::hash<int>{}(ote->getIndex());
+	return stdext::hash_value(ote->getIndex());
 }
 
 template<class _Kty,
