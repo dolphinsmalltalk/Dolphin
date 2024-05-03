58a323a2e61d5110260916ca1334fa002b0ba58f
Blair McGlashan <blairmcg@outlook.com>
2024-05-03 17:32:05 +0100

    Don't attempt to upload non-existent regression test results

--- UploadTestResults.ps1
+++ UploadTestResults.ps1
@@ -1,4 +1,7 @@
 # Powershell script to upload the regression tests results to AppVeyor
 
-$wc = New-Object 'System.Net.WebClient'
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\RegressionTests.xml))
+if ([System.IO.File]::Exists(".\RegressionTests.xml")) {
+	$wc = New-Object 'System.Net.WebClient'
+	$wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\RegressionTests.xml))
+}
+
