dolphinsmalltalk/Dolphin

commit a212761dacfe7112975e800c0ca5276f512ad1fa

Indexed decoding of UTF strings should be the same as stream decoding
In particular, handling of invalid sequences should behave in exactly the
same way.

Core/DolphinVM/StreamPrim.cpp
@@ -80,20 +80,12 @@ Oop* PRIMCALL Interpreter::primitiveNext(Oop* const sp, primargcount_t)
 						const Utf8String::CU* psz = reinterpret_cast<Utf8StringOTE*>(oteBuf)->m_location->m_characters;
 
 						SmallInteger codePoint;
-						// The macro (from icucommon.h) advances the index as well as calculating the code point
-						U8_NEXT(psz, index, static_cast<SmallInteger>(size), codePoint);
-
-						if (U_IS_UNICODE_CHAR(codePoint))
-						{
-							StoreCharacterToStack(sp, codePoint);
-							readStream->m_index = Integer::NewSigned32WithRef(index);
-							return sp;
-						}
-
-						return primitiveFailure(_PrimitiveFailureCode::IllegalCharacter);	// Malformed UTF-8, or at end of buffer over larger string, or at bad offset
+						// The macro (from icu.h) advances the index as well as calculating the code point
+						U8_NEXT_OR_FFFD(psz, index, static_cast<SmallInteger>(size), codePoint);
+						StoreCharacterToStack(sp, U_IS_UNICODE_NONCHAR(codePoint) ? Interpreter::UnicodeReplacementChar : codePoint);
+						readStream->m_index = Integer::NewSigned32WithRef(index);
+						return sp;
 					}
-
-					// Out of bounds
 					return primitiveFailure(_PrimitiveFailureCode::OutOfBounds);
 				}
 				break;
@@ -107,19 +99,12 @@ Oop* PRIMCALL Interpreter::primitiveNext(Oop* const sp, primargcount_t)
 					{
 						const Utf16String::CU* pwsz = oteString->m_location->m_characters;
 						SmallInteger codePoint;
-						// The macro (from icucommon.h) advances the index as well as calculating the code point
-						U16_NEXT(pwsz, index, size, codePoint);
-
-						if (U_IS_UNICODE_CHAR(codePoint) && !U16_IS_SURROGATE(codePoint))
-						{
-							StoreCharacterToStack(sp, codePoint);
-							readStream->m_index = Integer::NewSigned32WithRef(index);
-							return sp;
-						}
-
-						return primitiveFailure(_PrimitiveFailureCode::IllegalCharacter);	// Malformed UTF-16, or reading starting at an invalid offset
+						// The macro (from icu.h) advances the index as well as calculating the code point
+						U16_NEXT_OR_FFFD(pwsz, index, size, codePoint);
+						StoreCharacterToStack(sp, U_IS_UNICODE_NONCHAR(codePoint) ? Interpreter::UnicodeReplacementChar : codePoint);
+						readStream->m_index = Integer::NewSigned32WithRef(index);
+						return sp;
 					}
-
 					return primitiveFailure(_PrimitiveFailureCode::OutOfBounds);
 				}
 				break;
@@ -133,16 +118,10 @@ Oop* PRIMCALL Interpreter::primitiveNext(Oop* const sp, primargcount_t)
 					{
 						Utf32String::CU codePoint = oteString->m_location->m_characters[index];
 
-						if (U_IS_UNICODE_CHAR(codePoint))
-						{
-							StoreCharacterToStack(sp, codePoint);
-							readStream->m_index = Integer::NewSigned32WithRef(index + 1);
-							return sp;
-						}
-
-						return primitiveFailure(_PrimitiveFailureCode::IllegalCharacter);	// Invalid code point
+						StoreCharacterToStack(sp, U_IS_UNICODE_CHAR(codePoint) ? codePoint : Interpreter::UnicodeReplacementChar);
+						readStream->m_index = Integer::NewSigned32WithRef(index + 1);
+						return sp;
 					}
-
 					return primitiveFailure(_PrimitiveFailureCode::OutOfBounds);
 				}
 				break;

Core/DolphinVM/Interprt.h
@@ -620,6 +620,10 @@ public:
 	// String Class Primitives
 	static Oop* PRIMCALL primitiveStringAt(Oop* const sp, primargcount_t argCount);
 	static Oop* PRIMCALL primitiveStringAtPut(Oop* const sp, primargcount_t argCount);
+	
+	// UTF-8 decoding
+	static Oop* PRIMCALL primitiveUtf8StringDecodeAt(Oop* const sp, primargcount_t);
+	static Oop* PRIMCALL primitiveUtf8StringEncodedSizeAt(Oop* const sp, primargcount_t);
 
 	// Helper for memory moves
 	static void memmove(uint8_t* dst, const uint8_t* src, size_t count);

Core/DolphinVM/PrimitivesTable.cpp
@@ -235,8 +235,8 @@ extern "C" PRIMTABLEDECL Interpreter::PrimitiveFp primitivesTable[256] = {
 	Interpreter::primitiveBasicNextPut											, // case 222: WriteStream>>#basicNextPut:, FileStream|WriteStream>>#primitiveBasicNextPut:
 	Interpreter::primitiveStringAsUtf32String									, // case 223: Currently unimplemented/unused
 	Interpreter::primitiveBeginsWith											, // case 224: String>>#beginsWith:
-	Interpreter::unusedPrimitive												, // case 225:
-	Interpreter::unusedPrimitive												, // case 226:
+	Interpreter::primitiveUtf8StringDecodeAt									, // case 225: Utf8String>>decodeAt:
+	Interpreter::primitiveUtf8StringEncodedSizeAt								, // case 226: Utf8String>>encodedSizeAt:
 	Interpreter::primitiveOrdinalHashIgnoreCase									, // case 227: String>>#hashOrdinalsIgnoringCase:
 	Interpreter::primitiveStringLessOrEqual										, // case 228: String>>#<=
 	Interpreter::primitiveCharacterEquals										, // case 229: Character>>#=

Core/DolphinVM/strgprim.cpp
@@ -2337,3 +2337,62 @@ Oop* PRIMCALL Interpreter::primitiveStringAsUtf32String(Oop* const sp, primargco
 		return primitiveFailure(_PrimitiveFailureCode::AssertionFailure);
 	}
 }
+
+Oop* PRIMCALL Interpreter::primitiveUtf8StringDecodeAt(Oop* const sp, primargcount_t)
+{
+	Oop oopArg = *sp;
+	const Utf8StringOTE* oteReceiver = reinterpret_cast<const Utf8StringOTE*>(*(sp - 1));
+	if (ObjectMemoryIsIntegerObject(oopArg))
+	{
+		SmallUinteger i = ObjectMemoryIntegerValueOf(oopArg) - 1;
+		size_t length = oteReceiver->getSize();
+		if (i < length)
+		{
+			char32_t c;
+			char8_t* utf8 = oteReceiver->m_location->m_characters;
+			U8_NEXT_OR_FFFD(utf8, i, static_cast<SmallInteger>(length), c);
+			StoreCharacterToStack(sp - 1, U_IS_UNICODE_NONCHAR(c) ? Interpreter::UnicodeReplacementChar : c);
+			return sp - 1;
+		}
+		else
+		{
+			// Arg not a SmallInteger
+			return primitiveFailure(_PrimitiveFailureCode::OutOfBounds);
+		}
+	}
+	else
+	{
+		// Arg not a SmallInteger
+		return primitiveFailure(_PrimitiveFailureCode::InvalidParameter1);
+	}
+}
+
+Oop* PRIMCALL Interpreter::primitiveUtf8StringEncodedSizeAt(Oop* const sp, primargcount_t)
+{
+	Oop oopArg = *sp;
+	const Utf8StringOTE* oteReceiver = reinterpret_cast<const Utf8StringOTE*>(*(sp - 1));
+	if (ObjectMemoryIsIntegerObject(oopArg))
+	{
+		SmallUinteger i = ObjectMemoryIntegerValueOf(oopArg) - 1;
+		size_t length = oteReceiver->getSize();
+		if (i < length)
+		{
+			char32_t c;
+			SmallInteger j = i;
+			char8_t* utf8 = oteReceiver->m_location->m_characters;
+			U8_NEXT_OR_FFFD(utf8, j, static_cast<SmallInteger>(length), c);
+			*(sp - 1) = ObjectMemoryIntegerObjectOf(j - i);
+			return sp - 1;
+		}
+		else
+		{
+			// Arg not a SmallInteger
+			return primitiveFailure(_PrimitiveFailureCode::OutOfBounds);
+		}
+	}
+	else
+	{
+		// Arg not a SmallInteger
+		return primitiveFailure(_PrimitiveFailureCode::InvalidParameter1);
+	}
+}

Core/Object Arts/Dolphin/Base/Core.Dictionary.cls
@@ -458,9 +458,7 @@ printSelfEvaluatingOn: aStream
 				print: self associations single;
 				nextPut: $)]
 		ifFalse: 
-			[| assocs |
-			assocs := self associations.
-			aStream
+			[aStream
 				nextPutAll: #withAll:;
 				space;
 				print: (self sortSelfEvaluatingElements: self associations locale: aStream locale);

Core/Object Arts/Dolphin/Base/Core.PositionableStream.cls
@@ -111,7 +111,7 @@ lastPosition
 	^readLimit!
 
 locale
-	"Answer the <Locale> to be used when emitting or reaading localisable data to/from this stream."
+	"Answer the <Locale> to be used when emitting or reading localisable data to/from this stream."
 
 	^locale!
 

Core/Object Arts/Dolphin/Base/Core.String.cls
@@ -477,7 +477,7 @@ decodeNextAvailable: anInteger from: aReadStream
 	^newStream grabContents!
 
 decodeNextFrom: aReadStream
-	"Private - Answer the next <Character> in the receiver's encoding from the <ReadStream> argument ."
+	"Private - Answer the next <Character> in the receiver's encoding from the <ReadStream> argument, or the replacement character if the next sequence is invalid."
 
 	^self subclassResponsibility!
 
@@ -496,7 +496,7 @@ displayString
 	^self!
 
 encodedSizeAt: anInteger
-	"Private - Answer the <integer> number of code units occupied by the <Character> whose first code unit is at the specified index in the receiver. Throw an error if the code unit at anInteger is not either an ASCII code point or a UTF-8 lead surrogate"
+	"Private - Answer the <integer> number of code units occupied by the <Character> whose first code unit is at the specified index in the receiver."
 
 	^self subclassResponsibility!
 

Core/Object Arts/Dolphin/Base/Core.Utf16String.cls
@@ -124,7 +124,7 @@ decodeAt: anInteger
 					Character.Replacement]]
 		ifFalse: 
 			[surrogateBits == Character.Utf16TrailMask
-				ifTrue: [self errorIntraCharacterIndex: anInteger]
+				ifTrue: [Character.Replacement]
 				ifFalse: [Character utf16Value: leading ifInvalid: [Character.Replacement]]]!
 
 decodeNextFrom: aReadStream
@@ -174,16 +174,17 @@ encodedAt: anInteger put: aCharacter
 				ifFalse: [self class errorInvalidCodePoint: codePoint]]!
 
 encodedSizeAt: anInteger
-	"Private - Answer the <integer> number of code units occupied by the <Character> whose first code unit is at the specified index in the receiver. Throw an error if the code unit at anInteger is not either an ASCII code point or a UTF-8 lead surrogate"
+	"Private - Answer the <integer> number of code units occupied by the <Character> whose first code unit is at the specified index in the receiver."
 
-	| surrogateBits |
-	surrogateBits := (self basicAt: anInteger) bitAnd: Character.Utf16SurrogateMask.
-	^surrogateBits == Character.Utf16LeadMask
-		ifTrue: [2]
+	^(((self basicAt: anInteger) bitAnd: Character.Utf16SurrogateMask) == Character.Utf16LeadMask and: 
+			[anInteger < self size
+				and: [((self basicAt: anInteger + 1) bitAnd: Character.Utf16SurrogateMask) == Character.Utf16TrailMask]])
+		ifTrue: 
+			["Valid surrogate pair"
+			2]
 		ifFalse: 
-			[surrogateBits == Character.Utf16TrailMask
-				ifTrue: [self errorIntraCharacterIndex: anInteger]
-				ifFalse: [1]]!
+			["All other cases - single character, or invalid surrogate"
+			1]!
 
 encodedSizeOf: aCharacter
 	"Private - Answer the number of basic slots occupied by the argument when encoded in the UTF-16 encoding."
@@ -226,6 +227,9 @@ indexOfSubCollection: targetSequence startingAt: start
 
 isLeadCodeUnit: anInteger
 	^(anInteger bitAnd: Character.Utf16SurrogateMask) ~~ Character.Utf16TrailMask!
+
+isTrailSurrogate: anInteger
+	^(anInteger bitAnd: Character.Utf16SurrogateMask) == Character.Utf16TrailMask!
 
 maxEncoding
 	^2!
@@ -366,6 +370,7 @@ encodeOn:put:!encode/decode!private! !
 findString:startingAt:!public!searching! !
 indexOfSubCollection:startingAt:!public!searching! !
 isLeadCodeUnit:!encode/decode!private!testing! !
+isTrailSurrogate:!public! !
 maxEncoding!constants!private! !
 nextIndexOfCharacter:from:to:!private!searching! !
 replaceFrom:to:with:startingAt:!public!replacing! !

commit 403b405a2b93213c97be613036fef79c826dfcce

Fix decoding of last character on page boundary of UTF-8 file with BOM
Bug introduced by a212761dacfe7112975e800c0ca5276f512ad1fa, which changed
the primitiveNext to directly decode to U+FFFDs (replacement chars)
instead of failing on incorrect/incomplete encodings. We need the
primitive to fail on at least incomplete encodings in order that the
primitive can be used with buffers representing a partial range of data
from some underlying store, as otherwise incomplete characters at the
end of the buffer are converted to U+FFFDs rather than triggering
fallback processing that is able to read the encoded character
straddling a page boundary.

Core/DolphinVM/StreamPrim.cpp
@@ -80,11 +80,17 @@ Oop* PRIMCALL Interpreter::primitiveNext(Oop* const sp, primargcount_t)
 						const Utf8String::CU* psz = reinterpret_cast<Utf8StringOTE*>(oteBuf)->m_location->m_characters;
 
 						SmallInteger codePoint;
-						// The macro (from icu.h) advances the index as well as calculating the code point
-						U8_NEXT_OR_FFFD(psz, index, static_cast<SmallInteger>(size), codePoint);
-						StoreCharacterToStack(sp, U_IS_UNICODE_NONCHAR(codePoint) ? Interpreter::UnicodeReplacementChar : codePoint);
-						readStream->m_index = Integer::NewSigned32WithRef(index);
-						return sp;
+						// The macro (from icucommon.h) advances the index as well as calculating the code point
+						U8_NEXT(psz, index, static_cast<SmallInteger>(size), codePoint);
+
+						if (U_IS_UNICODE_CHAR(codePoint))
+						{
+							StoreCharacterToStack(sp, codePoint);
+							readStream->m_index = Integer::NewSigned32WithRef(index);
+							return sp;
+						}
+
+						return primitiveFailure(_PrimitiveFailureCode::IllegalCharacter);	// Malformed UTF-8, or at end of buffer over larger string, or at bad offset
 					}
 					return primitiveFailure(_PrimitiveFailureCode::OutOfBounds);
 				}
@@ -99,11 +105,17 @@ Oop* PRIMCALL Interpreter::primitiveNext(Oop* const sp, primargcount_t)
 					{
 						const Utf16String::CU* pwsz = oteString->m_location->m_characters;
 						SmallInteger codePoint;
-						// The macro (from icu.h) advances the index as well as calculating the code point
-						U16_NEXT_OR_FFFD(pwsz, index, size, codePoint);
-						StoreCharacterToStack(sp, U_IS_UNICODE_NONCHAR(codePoint) ? Interpreter::UnicodeReplacementChar : codePoint);
-						readStream->m_index = Integer::NewSigned32WithRef(index);
-						return sp;
+						// The macro (from icucommon.h) advances the index as well as calculating the code point
+						U16_NEXT(pwsz, index, size, codePoint);
+
+						if (U_IS_UNICODE_CHAR(codePoint) && !U16_IS_SURROGATE(codePoint))
+						{
+							StoreCharacterToStack(sp, codePoint);
+							readStream->m_index = Integer::NewSigned32WithRef(index);
+							return sp;
+						}
+
+						return primitiveFailure(_PrimitiveFailureCode::IllegalCharacter);	// Malformed UTF-16, or reading starting at an invalid offset
 					}
 					return primitiveFailure(_PrimitiveFailureCode::OutOfBounds);
 				}
@@ -160,7 +172,7 @@ Oop* PRIMCALL Interpreter::primitiveNext(Oop* const sp, primargcount_t)
 			return primitiveFailure(_PrimitiveFailureCode::NotSupported);		// Collection cannot be handled by primitive, rely on Smalltalk code
 		}
 
-		return primitiveFailure(_PrimitiveFailureCode::OutOfBounds);		// index not in range 0<index<limit
+		return primitiveFailure(_PrimitiveFailureCode::OutOfBounds);		// position not in range 0<position<readLimit
 	}
 
 	return primitiveFailure(_PrimitiveFailureCode::AssertionFailure);	// Receiver fails invariant check
@@ -657,7 +669,7 @@ Oop* PRIMCALL Interpreter::primitiveBasicNext(Oop* const sp, primargcount_t)
 			return primitiveFailure(_PrimitiveFailureCode::NotSupported);		// Collection cannot be handled by primitive, rely on Smalltalk code
 		}
 
-		return primitiveFailure(_PrimitiveFailureCode::OutOfBounds);		// index not in range 0<index<limit
+		return primitiveFailure(_PrimitiveFailureCode::OutOfBounds);		// position not in range 0<position<limit
 	}
 
 	return primitiveFailure(_PrimitiveFailureCode::AssertionFailure);	// Receiver fails invariant check

Core/Object Arts/Dolphin/Base/Core.File.cls
@@ -453,9 +453,14 @@ size: anInteger
 
 sniffEncoding
 	| ituResult buf count |
-	buf := ByteArray newFixed: SniffSize + 4.
+	buf := ByteArray newFixed: ##(SniffSize + 4).
 	self reset.
 	count := self read: buf count: SniffSize.
+	"If it starts with the UTF-8 BOM ( #[16rEF 16rBB 16rBF]), then we can be 99% certain it is UTF-8"
+	((buf at: 1) == 16rEF and: [(buf at: 2) == 16rBB and: [(buf at: 3) == 16rBF]])
+		ifTrue: 
+			[self position: 3.
+			^String.EncodingUtf8].
 	buf int32AtOffset: SniffSize put: -1.
 	^((AdvApi32
 		isTextUnicode: buf
@@ -470,19 +475,13 @@ sniffEncoding
 			[self position: ((ituResult allMask: IS_TEXT_UNICODE_SIGNATURE) ifTrue: [2] ifFalse: [0]).
 			String.EncodingUtf16]
 		ifFalse: 
-			["If it starts with the UTF-8 BOM ( #[16rEF 16rBB 16rBF]), then we can be 99% certain it is UTF-8"
-			(buf beginsWith: Utf8String.Bom)
-				ifTrue: 
-					[self position: 3.
-					String.EncodingUtf8]
-				ifFalse: 
-					[| utf8Stream |
-					self reset.
-					"Do a minimal evaluation of the unknown encoding. If the sniff block read as UTF-8 (allowing for that fact that we may have truncated the final character) then assume it is UTF-8, otherwise some ANSI code page."
-					utf8Stream := (Utf8String fromByteArray: buf) readStream.
-					((utf8Stream skipTo: Character.Replacement) not or: [utf8Stream atEnd])
-						ifTrue: [String.EncodingUtf8]
-						ifFalse: [String.EncodingAnsi]]]!
+			[| utf8Stream |
+			self reset.
+			"Do a minimal evaluation of the unknown encoding. If the sniff block read as UTF-8 (allowing for that fact that we may have truncated the final character) then assume it is UTF-8, otherwise some ANSI code page."
+			utf8Stream := (Utf8String fromByteArray: buf) readStream.
+			((utf8Stream skipTo: Character.Replacement) not or: [utf8Stream atEnd])
+				ifTrue: [String.EncodingUtf8]
+				ifFalse: [String.EncodingAnsi]]!
 
 spec: aString
 	"Private - Set the file path to the <readableString> argument."

Core/Object Arts/Dolphin/Base/Tests/Core.Tests.FileStreamTest.cls
@@ -73,6 +73,47 @@ tearDownTempStream
 
 	filestream file free!
 
+testAtEndWithUtf8Bom
+	(0 to: 1) , (FileStream.PageSize - $\xFEFF utf8Length - 3 to: FileStream.PageSize + 1)
+		, (FileStream.PageSize * 2 - $\xFEFF utf8Length - 3 to: FileStream.PageSize * 2 + 1) do: 
+				[:each |
+				| subject |
+				subject := self tempWriteStream: #utf8.
+				subject
+					position: each;
+					nextPut: $£.
+				self assert: subject position equals: each + 2.
+				self assert: subject atEnd.
+				subject position: each.
+				self assert: subject position equals: each.
+				self deny: subject atEnd.
+				self assert: subject basicNext equals: 194.
+				self assert: subject position equals: each + 1.
+				self deny: subject atEnd.
+				self assert: subject basicNext equals: 163.
+				self assert: subject position equals: each + 2.
+				self assert: subject atEnd.
+				subject position: each.
+				self deny: subject atEnd.
+				self assert: subject next equals: $£.
+				self assert: subject atEnd.
+				subject close.
+				subject := self tempReadStream.
+				subject position: each.
+				self assert: subject position equals: each.
+				self deny: subject atEnd.
+				self assert: subject basicNext equals: 194.
+				self assert: subject position equals: each + 1.
+				self deny: subject atEnd.
+				self assert: subject basicNext equals: 163.
+				self assert: subject position equals: each + 2.
+				self assert: subject atEnd.
+				subject position: each.
+				self deny: subject atEnd.
+				self assert: subject next equals: $£.
+				self assert: subject atEnd.
+				subject close]!
+
 testNextAvailableColonWithPaging
 	"#640"
 
@@ -227,6 +268,46 @@ testSingleByteOverflow
 	self assert: filestream next equals: $3.
 	filestream close!
 
+testUtf8StraddlingPageBoundary
+	"Test writing and then reading a multi-byte UTF-8 character that is straddling a page boundary"
+
+	| offset string subject allText |
+	subject := self tempWriteStream: #utf8.
+	self assert: subject atEnd.
+	offset := $\xFEFF utf8Length.
+	"Fill the first page up to the penultimate byte"
+	string := String new: FileStream.PageSize - offset - 1 withAll: $X.
+	subject nextPutAll: string.
+	self assert: subject atEnd.
+	subject nextPut: Character dolphin.
+	self assert: subject atEnd.
+	subject close.
+	allText := string copyWith: Character dolphin.
+	self assert: (File readAllText: tempFileName) equals: allText.
+	subject := self tempReadStream.
+	self deny: subject atEnd.
+	self assert: subject encoding equals: #utf8.
+	self assert: subject upToEnd equals: allText.
+	subject reset.
+	self assert: (subject next: string size) equals: string.
+	subject reset.
+	string size timesRepeat: 
+			[self assert: subject next equals: $X.
+			self deny: subject atEnd].
+	"The emoji straddles the page boundary"
+	self assert: subject next equals: Character dolphin.
+	self assert: subject atEnd.
+	subject reset.
+	subject position: string size.
+	self deny: subject atEnd.
+	self assert: subject nextAvailable equals: Character dolphin.
+	subject position: string size.
+	Character dolphin asUtf8String asByteArray do: 
+			[:each |
+			self deny: subject atEnd.
+			self assert: subject basicNext equals: each].
+	self assert: subject atEnd!
+
 testVeryLargeSparseFile
 	| empty max |
 	filestream := self tempWriteStream.
@@ -291,10 +372,12 @@ streamClassSupportsUtf16!helpers!private! !
 streamOn:!helpers!private! !
 streamOnFile:type:!helpers!private! !
 tearDownTempStream!helpers!private! !
+testAtEndWithUtf8Bom!public!unit tests! !
 testNextAvailableColonWithPaging!public!unit tests! !
 testNextPutAll!public!unit tests! !
 testOwnsFile!public! !
 testSingleByteOverflow!public!unit tests! !
+testUtf8StraddlingPageBoundary!public!unit tests! !
 testVeryLargeSparseFile!public!unit tests! !
 testWritePastEnd!public!unit tests! !
 !

Core/Object Arts/Dolphin/Base/Tests/Core.Tests.FileTest.cls
@@ -737,7 +737,7 @@ testShortPath
 testSniffEncodingAnsi
 	tempFile := File temporary.
 	{
-		AnsiString fromByteArray: Utf16String.Bom , 'odd' asByteArray.
+		AnsiString fromByteArray: $\xFEFF asUtf16String asByteArray , 'odd' asByteArray.
 		'£' asAnsiString.
 		' informações'.
 		'süß«'.
@@ -751,11 +751,13 @@ testSniffEncodingAnsi
 				self assert: tempFile position equals: 0]!
 
 testSniffEncodingUtf16
+	| utf16Bom |
 	tempFile := File temporary.
+	utf16Bom := $\xFEFF asUtf16String asByteArray.
 	{
-		Utf16String.Bom.
-		Utf16String.Bom , 'even' asAnsiString asByteArray.
-		Utf16String.Bom , Character dolphin asUtf8String asByteArray.
+		utf16Bom.
+		utf16Bom , 'even' asAnsiString asByteArray.
+		utf16Bom , Character dolphin asUtf8String asByteArray.
 		('a ' asAnsiString , Character dolphin asUtf16String) asByteArray.
 		'a £' asUtf16String asByteArray
 	} do: 
@@ -765,15 +767,17 @@ testSniffEncodingUtf16
 					write: each.
 				self assert: tempFile sniffEncoding equals: String.EncodingUtf16.
 				self assert: tempFile position
-					equals: ((each beginsWith: Utf16String.Bom) ifTrue: [Utf16String.Bom size] ifFalse: [0])]!
+					equals: ((each beginsWith: utf16Bom) ifTrue: [utf16Bom size] ifFalse: [0])]!
 
 testSniffEncodingUtf8
 	"The sniffing will regard short sequences as most likely to be UTF-8 if they can be decoded as UTF-8, even if they in fact ANSI text. If the content is pure ASCII, then it makes no difference anyway as both UTF-8 and ANSI encodings (with any code page) are the same for this case."
 
+	| utf8Bom |
 	tempFile := File temporary.
+	utf8Bom := $\xFEFF asUtf8String asByteArray.
 	{
-		Utf8String.Bom.
-		Utf8String.Bom , '£' asAnsiString asByteArray.
+		utf8Bom.
+		utf8Bom , '£' asAnsiString asByteArray.
 		'a b c ' asByteArray.
 		$£ asUtf8String asByteArray.
 		'a 🐬' asUtf8String asByteArray
@@ -784,7 +788,7 @@ testSniffEncodingUtf8
 					write: each.
 				self assert: tempFile sniffEncoding equals: String.EncodingUtf8.
 				self assert: tempFile position
-					equals: ((each beginsWith: Utf8String.Bom) ifTrue: [Utf8String.Bom size] ifFalse: [0])]!
+					equals: ((each beginsWith: utf8Bom) ifTrue: [utf8Bom size] ifFalse: [0])]!
 
 testSplitDriveFrom
 	self should: [File splitDriveFrom: nil] raise: Error.